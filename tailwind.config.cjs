2c8a1b94ab0ff1e07222c2226af36b2010a8355c
SergioD <sergio_david2109@hotmail.com>
2023-06-19 20:05:25 -0600

    refactor: add color palette

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sansSerif: ['Source Sans Pro', 'sans-serif'],
+      mono: ['Source Code Pro', 'monospace'],
+      handwriting: ['Kalam', 'cursive']
+    },
+    extend: {
+      colors: {
+        purpleDark: '#170127',
+        purpleWine: '#300b3f',
+        purpleScarf: '#420c58',
+        purpleLight: '#a15baa',
+        pinkSolid: '#e3a4a8',
+        yellowFizz: '#dfb77d',
+      },
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
 
