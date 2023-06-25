68896fde979288bb35650e7c0915b5b46d846f26
mutska <mutska@ciencias.unam.mx>
2023-06-25 11:56:29 -0600

    feat(components): add Footer Component

---
+++ src/lib/components/Footer.component.svelte
@@ -0,0 +1,19 @@
+<div class="py-4 px-9 flex flex-col items-center gap-y-2 bg-purpleGray rounded-2xl m-auto max-h-fit">
+	<h1 class="text-white text-xl font-bold">Scribbles Team</h1>
+    <div class="flex flex-col items-center gap-y-0">
+    <span class="text-white">user@mail.com</span>
+    <span class="text-white">user@mail.com</span>
+  </div>
+	<div class="flex flex-row gap-x-2 items-start">
+    <a href="/privacy">
+		<span class="text-white">Privacy</span>
+    </a>
+    <span class="text-white">&middot;</span>
+    <a href="/cookies">
+		<span class="text-white">Cookies</span>
+    </a>
+    <span class="text-white">&middot;</span>
+    <span class="text-white">&copy;</span>
+		<span class="text-white">2023</span>
+	</div>
+</div>
