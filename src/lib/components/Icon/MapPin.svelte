544d1bb460556f5c983ffcc550592baf3103a0f8
SergioD <sergio_david2109@hotmail.com>
2023-06-25 14:27:07 -0600

    feat(components): add icon components

---
+++ src/lib/components/Icon/MapPin.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let width: number = 25, height: number = 25;
+</script>
+
+<svg {width} {height} viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_340_732)">
+<path d="M21.425 10.0625C21.425 17.0625 12.425 23.0625 12.425 23.0625C12.425 23.0625 3.42505 17.0625 3.42505 10.0625C3.42505 7.67555 4.37326 5.38637 6.06109 3.69854C7.74891 2.01071 10.0381 1.0625 12.425 1.0625C14.812 1.0625 17.1012 2.01071 18.789 3.69854C20.4768 5.38637 21.425 7.67555 21.425 10.0625Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M12.425 13.0625C14.0819 13.0625 15.425 11.7194 15.425 10.0625C15.425 8.40565 14.0819 7.0625 12.425 7.0625C10.7682 7.0625 9.42505 8.40565 9.42505 10.0625C9.42505 11.7194 10.7682 13.0625 12.425 13.0625Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<defs>
+<clipPath id="clip0_340_732">
+<rect width="24" height="24" fill="white" transform="translate(0.425049 0.0625)"/>
+</clipPath>
+</defs>
+</svg>
