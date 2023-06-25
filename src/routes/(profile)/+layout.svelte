8a8a552310aeda5299165d5c314b39d5e1b76d00
mutska <mutska@ciencias.unam.mx>
2023-06-25 15:40:47 -0600

    feat(profile): add profile layout

--- src/routes/(profile)/+layout.svelte
+++ src/routes/(profile)/+layout.svelte
@@ -1,3 +1,3 @@
-<div class="grid min-h-screen grid-cols-12">
+<div class="grid min-h-screen grid-cols-12 px-[70px] bg-purpleDark gap-x-5">
 <slot><!-- optional fallback --></slot>
 </div>
---
+++ src/routes/(profile)/profile/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { NavBar } from '$components';
+	import { CoverPhoto } from '$components';
+	import { ProfilePhoto } from '$components';
+	import { UserDescription } from '$components';
+	import { Post } from '$components';
+
+	let isLogin: boolean = false;
+	const post_data = {
+		user_photo_url:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJr94g26anSnYVgCnaDlZvKLRA51Fk6HLIsw&usqp=CAU',
+		user_name: 'mutska',
+		user_url: '#',
+		post_url: '#',
+		posted_ago: '1d',
+		post_content: 'Este es un ejemplo del texto de un post',
+		post_thumbnail_url:
+			'https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg',
+		like_on: true,
+		likes_count: 65,
+		comments_count: 10,
+		tags_count: 3
+	};
+	let num_posts = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+</script>
+
+<div class="col-span-2">
+	<div class="mt-8">
+		<NavBar />
+	</div>
+</div>
+<div class="col-span-10">
+	<div class="flex flex-col gap-y-4 mt-8">
+    <CoverPhoto />
+		<div class="flex flex-row gap-x-8">
+      <ProfilePhoto />
+			<UserDescription />
+		</div>
+	</div>
+	<div class="flex flex-col gap-y-8">
+		{#each num_posts as post}
+			<Post {...post_data} />
+		{/each}
+	</div>
+</div>
