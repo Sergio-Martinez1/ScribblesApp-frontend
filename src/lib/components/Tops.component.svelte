5eaee3fb18033eaac2b93e2bdb6ff91fb2847fa3
mutska <mutska@ciencias.unam.mx>
2023-06-25 11:54:00 -0600

    feat(components): add Tops Component
    The tops component shows the top 3 tags in posts

---
+++ src/lib/components/Tops.component.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let top_one: string = ""; 
+  export let top_two: string = ""; 
+  export let top_three: string = ""; 
+</script>
+
+
+<div class="py-4 px-9 flex flex-col items-center gap-y-2 bg-purpleGray rounded-2xl m-auto max-h-fit">
+	<h1 class="text-white text-xl font-bold">On time for</h1>
+    <div class="flex flex-col items-center gap-y-1">
+    <div class="flex flex-col items-start gap-y-0 bg-purpleLight">
+    <h1 class="text-white">{top_one}</h1>
+    </div>
+    <div class="flex flex-col items-start gap-y-0 bg-purpleLight">
+    <h1 class="text-white">{top_two}</h1>
+    </div>
+    <div class="flex flex-col items-start gap-y-0 bg-purpleLight">
+    <h1 class="text-white">{top_three}</h1>
+    </div>
+  </div>
+</div>
