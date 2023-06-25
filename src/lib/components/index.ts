75c48c2f99bdc3cd83d641738c405da8b532b80c
mutska <mutska@ciencias.unam.mx>
2023-06-25 11:54:43 -0600

    feat(chores): add index file for components

---
+++ src/lib/components/index.ts
@@ -0,0 +1,14 @@
+export {default as Login} from "./Login.component.svelte";
+export {default as Signup} from "./Signup.component.svelte";
+export {default as Footer} from "./Footer.component.svelte";
+export {default as Tops} from "./Tops.component.svelte";
+export {default as CreateTags} from "./CreateTags.component.svelte";
+export {default as LoginInHome} from "./LoginInHome.svelte";
+export {default as NavBar} from "./NavBar.component.svelte";
+export {default as Post} from "./Post.component.svelte";
+export {default as PostMetadata} from "./PostMetadata.component.svelte";
+export {default as PostOptions} from "./PostOptions.component.svelte";
+export {default as PublicationBar} from "./PublicationBar.component.svelte";
+export {default as Reactions} from "./Reactions.component.svelte";
+export {default as Tags} from "./Tags.component.svelte";
+export {default as TextAreaAutosize} from "./TextAreaAutosize.svelte";
