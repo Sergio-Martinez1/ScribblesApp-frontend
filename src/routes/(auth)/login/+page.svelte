a4be0cad04ff06a20c879202f4e4a0e02a5101b8
mutska <mutska@ciencias.unam.mx>
2023-06-25 12:04:26 -0600

    feat(login): add login page

---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import {Login} from "$components";
+</script>
+
+<div class="col-span-6  bg-krispyPurple flex">
+  <Login />
+</div>
+<div class="col-span-6  bg-black flex">
+
+<div class="bg-krispyPurple m-auto border-solid border-2 rounded-3xl border-krispyPurple">
+
+    <div class="flex flex-col gap-y-5 items-start px-8 py-9">
+    <h1 class="text-4xl font-bold">Are you new in <br> Scribbles?</h1>
+    <p class="font-bold">
+      Join to the newest platform where <br> you can share all your moments
+    </p>
+      <a href="/signup">
+      <button class="bg-black text-krispyPurple rounded-xl w-36 h-12">
+      <span class="px-auto font-bold">Sign Up</span>
+      </a>
+    </div>
+</div>
+
+</div>
