b18e3beb2987ea59a61f96214e2f27a89681e9e6
mutska <mutska@ciencias.unam.mx>
2023-06-27 21:19:45 -0600

    feat(signup): add signup functionality

---
+++ src/routes/(auth)/signup/+page.server.ts
@@ -0,0 +1,48 @@
+import { fail, type Actions, redirect } from "@sveltejs/kit";
+import { env } from "$env/dynamic/private";
+
+export const actions: Actions = {
+  signup: async ({ request, fetch }) => {
+    
+    const form = await request.formData();
+    const username = form.get("username");
+    const email = form.get("email");
+    const password = form.get("password");
+    const confirm = form.get("confirm");
+    if (!username) {
+      return fail(400, { usernameMissing: true, email });
+    }
+    if (!email) {
+      return fail(400, { emailMissing: true, username });
+    }
+    if (!password) {
+      return fail(400, { passwordMissing: true, username, email});
+    }
+    if (!confirm) {
+      return fail(400, { passwordConfirmMissing: true, username, email});
+    }
+    if (password !== confirm) {
+      return fail(400, { passwordConfirm: true, username,email});
+    }
+
+    const base_api_url: string = env.API_URL;
+    const url = `${base_api_url}/users/signup`;
+
+    const body = {
+      username,
+      email,
+      password,
+    };
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+    const response = await fetch(url, options);
+    if (response.ok) {
+      throw redirect(303, "/login");
+    }
+  },
+};
--- src/routes/(auth)/signup/+page.svelte
+++ src/routes/(auth)/signup/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+  import type { PageData, ActionData } from './$types';
   import {Signup} from "$components";
+  export let form: ActionData;
 </script>
 
 <div class="col-span-full  bg-black flex">
-  <Signup />
+  <Signup {form} />
 </div>
