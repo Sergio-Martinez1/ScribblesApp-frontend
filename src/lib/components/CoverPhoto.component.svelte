766f1ef6455681a13b33797a5a7d3a7119aa09be
SergioD <sergio_david2109@hotmail.com>
2023-06-30 20:30:41 -0600

    refactor(components): add verification for user and remove static data

--- src/lib/components/CoverPhoto.component.svelte
+++ src/lib/components/CoverPhoto.component.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Image from '$lib/components/Icon/Image.svelte';
-	export let coverPhotoUrl: string =
-		'https://images.unsplash.com/photo-1483884105135-c06ea81a7a80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y292ZXIlMjBnaXJsfGVufDB8fDB8fHww&w=1000&q=80';
+	export let coverPhotoUrl: string = '';
+	export let editable: boolean = false;
 </script>
 
 <div class="w-full h-[283px] relative">
@@ -10,10 +10,12 @@
 	{:else}
 		<div class="w-full h-full rounded-2xl bg-purpleGray" />
 	{/if}
-	<button
-		class="bg-krispyPurple hover:bg-lessPurple active:bg-krispyPurple rounded-full flex items-center justify-center px-4 py-2 absolute bottom-5 right-5 gap-3"
-	>
-		<div class="w-[20px] h-[20px]"><Image stroke="#ffffff" /></div>
-		<p class="text-white font-bold">Edit cover photo</p></button
-	>
+	{#if editable}
+		<button
+			class="bg-krispyPurple hover:bg-lessPurple active:bg-krispyPurple rounded-full flex items-center justify-center px-4 py-2 absolute bottom-5 right-5 gap-3"
+		>
+			<div class="w-[20px] h-[20px]"><Image stroke="#ffffff" /></div>
+			<p class="text-white font-bold">Edit cover photo</p></button
+		>
+	{/if}
 </div>
