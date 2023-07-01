e93637a6bfee530ae54b1fe511d2b01878dde9b5
SergioD <sergio_david2109@hotmail.com>
2023-06-30 20:37:23 -0600

    refactor(components): add verification for user and remove static data

--- src/lib/components/ProfilePhoto.component.svelte
+++ src/lib/components/ProfilePhoto.component.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import User from '$lib/components/Icon/User.svelte';
 	import Camera from '$lib/components/Icon/Camera.svelte';
-	export let username: string = 'Test username';
-	export let profilePhoto: string =
-		'https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg';
+	export let username: string = '';
+	export let profilePhoto: string = '';
+	export let editable: boolean = false;
 </script>
 
 <div class="w-[174px] h-fit">
@@ -13,10 +13,12 @@
 		{:else}
 			<User width={174} height={174} />
 		{/if}
-		<button
-			class="bg-krispyPurple hover:bg-lessPurple active:bg-krispyPurple rounded-full flex items-center justify-center p-2 absolute bottom-2 right-2"
-			><div class="w-[24px] h-[24px]"><Camera /></div></button
-		>
+		{#if editable}
+			<button
+				class="bg-krispyPurple hover:bg-lessPurple active:bg-krispyPurple rounded-full flex items-center justify-center p-2 absolute bottom-2 right-2"
+				><div class="w-[24px] h-[24px]"><Camera /></div></button
+			>
+		{/if}
 	</div>
 	<p class="text-white font-bold w-fit mx-auto text-lg">{username}</p>
 </div>
