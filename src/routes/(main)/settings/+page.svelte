52d63d2a0d40d25c156070878b4185ee75bf14c1
mutska <mutska@ciencias.unam.mx>
2023-06-30 21:08:07 -0600

    feat(settings): add component to settings page
    - integrate ChangeUsername Component in the settings page

--- src/routes/(main)/settings/+page.svelte
+++ src/routes/(main)/settings/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import type { ActionData, PageData } from './$types';
 	export let form: ActionData;
-	import { ChangePassword, DeleteAccount, EditProfile } from '$components';
+	import { ChangePassword, ChangeUsername, 
+    DeleteAccount, EditProfile } from '$components';
   let edit_profile_view = true;
 	let password_view: boolean = false;
+	let username_view: boolean = false;
 	let toggle_theme_view: boolean = false;
   export let data: PageData;
   $: user = data.user;
@@ -11,11 +13,12 @@
 </script>
 
 <div class="col-span-5 flex flex-col items-center gap-y-4">
-	<h1 class="text-white font-bold text-xl mt-3">Your Account</h1>
+	<h1 class="text-white font-bold text-xl mt-3">Your Account {user.username}</h1>
 	<button
 		class="bg-purpleLight min-w-full text-center rounded-2xl py-3 hover:cursor-pointer"
 		on:click={() => {
       edit_profile_view = true;
+      username_view = false;
 			password_view = false;
 			toggle_theme_view = false;
 		}}
@@ -26,6 +29,18 @@
 		class="bg-purpleLight min-w-full text-center rounded-2xl py-3 hover:cursor-pointer"
 		on:click={() => {
       edit_profile_view = false;
+			username_view = true;
+			password_view = false;
+			toggle_theme_view = false;
+		}}
+	>
+		<h1 class="text-white text-lg">Change your username</h1>
+	</button>
+	<button
+		class="bg-purpleLight min-w-full text-center rounded-2xl py-3 hover:cursor-pointer"
+		on:click={() => {
+      edit_profile_view = false;
+			username_view = false;
 			password_view = true;
 			toggle_theme_view = false;
 		}}
@@ -36,6 +51,7 @@
 		class="bg-purpleLight min-w-full text-center rounded-2xl py-3 hover:cursor-pointer"
 		on:click={() => {
       edit_profile_view = false;
+			username_view = false;
 			password_view = false;
 			toggle_theme_view = true;
 		}}
@@ -46,6 +62,7 @@
 		class="bg-purpleLight min-w-full text-center rounded-2xl py-3 cursor-pointer"
 		on:click={() => {
 			edit_profile_view = false;
+			username_view = false;
 			password_view = false;
 			toggle_theme_view = false;
 		}}
@@ -56,6 +73,8 @@
 <div class="col-span-5 flex flex-col items-center gap-y-4">
 	{#if edit_profile_view}
     <EditProfile {form} {...user} />
+	{:else if username_view}
+		<ChangeUsername {form} username={user.username}/>
 	{:else if password_view}
 		<ChangePassword {form} />
 	{:else if toggle_theme_view}
