937ad77ae8a591a7cec9cc310855ae9583a5113d
SergioD <sergio_david2109@hotmail.com>
2023-06-30 20:40:32 -0600

    refactor(components): change exported variables and styles
    Change the exported variables based on api data names and change styles
    for improve the way to display data.

--- src/lib/components/Post.component.svelte
+++ src/lib/components/Post.component.svelte
@@ -2,43 +2,62 @@
 	import UserIcon from '$lib/components/Icon/User.svelte';
 	import Reactions from '$lib/components/Reactions.component.svelte';
 	import PostOptions from '$lib/components/PostOptions.component.svelte';
+	import { calculate_posted_time } from '$lib/utils/calculate_posted_time';
 
+	//POST
 	export let user_photo_url: string;
 	export let user_name: string = 'user';
 	export let user_url: string = '#';
-
 	export let post_url: string = '#';
-	export let posted_ago: string = '1d';
+	export let publication_date: string = '0d';
 	export let post_content: string = '';
 	export let post_thumbnail_url: string;
+  export let post_id: number;
 
 	// REACTIONS
 	export let like_on: boolean = false;
 	export let likes_count: number = 0;
 	export let comments_count: number = 0;
 	export let tags_count: number = 0;
+	export let vertical: boolean = false;
+	export let tags: string[];
+	export let post_by_tags_url: string = '/posts';
 
-	let editable: boolean;
+	//POST OPTIONS
+	export let creator_id: number;
+	export let myUser_id: number;
 </script>
 
 <div class="bg-purpleGray rounded-2xl">
 	<div class="flex items-center justify-between px-2.5 pt-2.5">
 		<a href={user_url} class="w-[59px] h-[58px] rounded-full overflow-hidden">
 			{#if user_photo_url}
-				<img src={user_photo_url} alt="User" />
+				<img class="w-full h-full object-cover" src={user_photo_url} alt="User" />
 			{:else}
 				<UserIcon />
 			{/if}
 		</a>
 		<div class="bg-purpleLight h-fit rounded-2xl ml-2.5">
-			<p class="px-3 py-1 text-white font-bold">{user_name} · {posted_ago}</p>
+			<p class="px-3 py-1 text-white font-bold">
+				{user_name} · {calculate_posted_time(publication_date)}
+			</p>
 		</div>
 		<div class="grow" />
 		<div class="mr-3">
-			<Reactions {like_on} {likes_count} {comments_count} {tags_count} {post_url} />
+			<Reactions
+				{like_on}
+				{likes_count}
+				{comments_count}
+				{tags_count}
+				{post_url}
+				{vertical}
+				{tags}
+				{post_by_tags_url}
+        {post_id}
+			/>
 		</div>
 		<div class="mr-7">
-			<PostOptions bind:editable />
+			<PostOptions {myUser_id} {creator_id} />
 		</div>
 	</div>
 	<div class="flex px-5 py-2.5">
@@ -53,7 +72,7 @@
 			{/if}
 		</a>
 	</div>
-  
+
 	<dialog class="bg-purpleGray rounded-2xl shadow-[0px_0px_10px_1px_black]" id="delete-dialog">
 		<form>
 			<p class="text-white font-bold mb-2 mx-auto w-fit text-lg">Delete post?</p>
