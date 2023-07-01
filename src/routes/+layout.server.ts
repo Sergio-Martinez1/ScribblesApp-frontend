667f7e9c8f8ac110c2d68d69cf9f6eb415ec3d84
SergioD <sergio_david2109@hotmail.com>
2023-06-30 21:05:44 -0600

    refactor: add profile photo in returned data

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,10 +4,12 @@ export const load: LayoutServerLoad = async ({ cookies }) => {
   const access_token = cookies.get('access_token');
   if (access_token) {
     const username = cookies.get('username') as string;
-    let id = cookies.get('id') as string;
+    const id = cookies.get('id') as string;
+    const profile_photo = cookies.get('profile_photo');
     return {
       username,
-      id
+      id,
+      profile_photo,
     };
   }
 };
