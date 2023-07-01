5e40ae75afdf7d9f567270d3589b8bcfd1636ea4
SergioD <sergio_david2109@hotmail.com>
2023-06-30 20:59:04 -0600

    refactor: remove static data and bind the login state for navBar

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,17 +1,14 @@
 <script lang="ts">
   import type { LayoutData } from "./$types";
 	import NavBar from '$components/NavBar.component.svelte';
+
+  export let data: LayoutData;
+	$: isLogin = data.username ? true : false;
 </script>
 
 <div class="bg-purpleDark min-h-screen">
 	<div class="grid grid-cols-12 px-[min(6%,_70px)] gap-[20px] max-w-7xl mx-auto">
-		<div class="col-span-2 mt-8"><NavBar login={true} /></div>
+		<div class="col-span-2 mt-8"><NavBar login={isLogin} /></div>
 		<slot><!-- optional fallback --></slot>
 	</div>
 </div>
-
-<!-- <style> -->
-<!--   div div { -->
-<!--     padding-left: min(5%, 70px); -->
-<!--   } -->
-<!-- </style> -->
