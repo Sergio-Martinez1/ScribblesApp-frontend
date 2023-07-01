3e6cfba50772ad62a71cffb3dea264e92545f514
SergioD <sergio_david2109@hotmail.com>
2023-06-30 20:50:45 -0600

    refactor(components): add a form and connect to tags component
    Add a form for handle post and delete reactions and connect the
    component to tags component via props.

--- src/lib/components/Reactions.component.svelte
+++ src/lib/components/Reactions.component.svelte
@@ -5,6 +5,8 @@
 	import { fly } from 'svelte/transition';
 	import Tags from '$lib/components/Tags.component.svelte';
 	import { clickOutside } from '$lib/utils/clickOutside';
+	import { applyAction, enhance } from '$app/forms';
+	import type { SubmitFunction } from '@sveltejs/kit';
 
 	export let like_on: boolean = false;
 	export let likes_count: number = 0;
@@ -12,17 +14,26 @@
 	export let tags_count: number = 0;
 	export let post_url: string = '#';
 	export let vertical: boolean = false;
+	export let tags: string[];
+	export let post_by_tags_url: string = '/posts';
+	export let post_id: number;
 
-	let like_fill: string = 'none';
+	let like_fill: string = like_on ? '#2C41FF' : 'none';
 	let like_stroke: string = like_on ? '#2C41FF' : '#ffffff';
-	export let tag_toogle: boolean = false;
+	let tag_toogle: boolean = false;
 
 	function toggle_like() {
 		like_on = !like_on;
 		likes_count = like_on ? likes_count + 1 : likes_count - 1;
-		// like_fill = like_on ? '#2C41FF' : 'none';
+		like_fill = like_on ? '#2C41FF' : 'none';
 		like_stroke = like_on ? '#2C41FF' : '#ffffff';
 	}
+
+  const notReload: SubmitFunction = () => {
+    return async ({result}) => {
+      await applyAction(result);
+    }
+  }
 
 	function handleClickOutside(event: CustomEvent) {
 		tag_toogle = false;
@@ -30,19 +41,22 @@
 </script>
 
 <div class="bg-purpleLight flex rounded-2xl px-2 relative {vertical ? 'gap-2 py-1.5' : 'py-2'}">
-	<div
-		on:keypress={() => {}}
-		on:click={toggle_like}
-		class="like cursor-pointer {vertical ? 'flex-col items-center' : ''}"
-	>
-		<div class="w-[20px]">
-			<Like fill={like_fill} stroke={like_stroke} />
-		</div>
-		<!-- Likes Count -->
-		{#key likes_count}
-			<p in:fly|local={{ y: 10 }} class:active={like_on}>{likes_count}</p>
-		{/key}
-	</div>
+	<form method="POST" action="/home?/toogle_reaction" use:enhance={notReload}>
+		<input type="hidden" value={post_id} name="post_id" />
+		<input type="hidden" value={like_on} name="state_on" />
+		<button
+			on:click={toggle_like}
+			class="like cursor-pointer flex {vertical ? 'flex-col items-center' : ''}"
+		>
+			<div class="w-[20px]">
+				<Like fill={like_fill} stroke={like_stroke} />
+			</div>
+			<!-- Likes Count -->
+			{#key likes_count}
+				<p in:fly|local={{ y: 10 }} class:active={like_on}>{likes_count}</p>
+			{/key}
+		</button>
+	</form>
 
 	<!-- Comments -->
 	<a href={post_url} class="message {vertical ? 'flex-col items-center' : ''}">
@@ -79,14 +93,15 @@
 	<!-- TOGGLE TAG COMPONENT -->
 	{#if tag_toogle}
 		<div in:fly|local={{ y: 10 }} class="absolute top-9 right-2 z-10">
-			<Tags />
+			<Tags {tags} posts_url={post_by_tags_url} />
 		</div>
 	{/if}
 </div>
 
 <style lang="postcss">
 	div div,
-	div a {
+	div a,
+	div form {
 		@apply mx-1.5 flex;
 	}
 	p {
