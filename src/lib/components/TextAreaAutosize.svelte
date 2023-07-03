5abced66a2df911aea5ddda7020b584555a2d16c
SergioD <sergio_david2109@hotmail.com>
2023-07-02 21:49:41 -0600

    refactor(components): add prop for max size of component

--- src/lib/components/TextAreaAutosize.svelte
+++ src/lib/components/TextAreaAutosize.svelte
@@ -9,15 +9,18 @@
 	export let focus_text: boolean = false;
 	//STYLES
 	export let placeHolder: string = "What's happening?";
+  export let maxSize: string = "";
 
 	$: outputText = innerText.replace(/\n{3,}/g, '\n\n');
 </script>
 
 <div class="relative">
 	{#if showPlaceHolder}
-		<p class="text-white opacity-50 absolute top-[50%] translate-y-[-50%]">{placeHolder}</p>
+		<p class="text-white opacity-50 absolute top-0">{placeHolder}</p>
 	{/if}
 	<div
+		class="text-bar text-white outline-none break-all relative overflow-y-auto max-h-80"
+    style="height: {maxSize};"
 		bind:innerText
 		on:input={() => {
 			showPlaceHolder =
@@ -33,6 +36,5 @@
 			focus_text = false;
 		}}
 		contenteditable="true"
-		class="text-bar text-white outline-none break-all relative overflow-y-auto max-h-80"
 	/>
 </div>
