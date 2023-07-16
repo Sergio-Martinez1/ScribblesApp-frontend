c93bea36f9145b22c642e0aba65caf2d3198a077
SergioD <sergio_david2109@hotmail.com>
2023-07-16 16:29:12 -0600

    chore(layout): change nav bar to have position fixed

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
-  import type { LayoutData } from "./$types";
+	import type { LayoutData } from './$types';
 	import NavBar from '$components/NavBar.component.svelte';
 
-  export let data: LayoutData;
+	export let data: LayoutData;
 	$: isLogin = data.username ? true : false;
 </script>
 
 <div class="bg-purpleDark min-h-screen">
 	<div class="grid grid-cols-12 px-[min(6%,_70px)] gap-[20px] max-w-7xl mx-auto">
-		<div class="col-span-2 mt-8"><NavBar login={isLogin} /></div>
+		<div class="col-span-2 mt-8">
+      <div class="relative w-full">
+			<div class="fixed">
+				<NavBar login={isLogin} />
+			</div>
+        
+      </div>
+		</div>
 		<slot><!-- optional fallback --></slot>
 	</div>
 </div>
