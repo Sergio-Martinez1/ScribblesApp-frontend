2bd3cdadfc29faac79c6502c1808ea0c1fd0e483
SergioD <sergiod@DOS.sergiod>
2023-07-30 19:38:20 -0600

    refactor: change styles and name variables

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -3,18 +3,16 @@
 	import NavBar from '$components/NavBar.component.svelte';
 
 	export let data: LayoutData;
-	$: isLogin = data.username ? true : false;
+  $: myUser = data.plainMyUser;
+	$: isLogin = myUser ? true : false;
 </script>
 
 <div class="bg-purpleDark min-h-screen">
-	<div class="grid grid-cols-12 px-[min(6%,_70px)] gap-[20px] max-w-7xl mx-auto">
-		<div class="col-span-2 mt-8">
-      <div class="relative w-full">
-			<div class="fixed">
+	<div class="grid grid-cols-12 px-[40px] gap-[20px] max-w-7xl mx-auto">
+		<div class="col-span-2 mt-8 relative">
+			<div class="fixed w-[174px]">
 				<NavBar login={isLogin} />
 			</div>
-        
-      </div>
 		</div>
 		<slot><!-- optional fallback --></slot>
 	</div>
