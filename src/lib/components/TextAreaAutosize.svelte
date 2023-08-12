7776f91ad1e8b59584f482d77841676231e349a0
SergioD <sergiod@DOS.sergiod>
2023-08-11 22:46:54 -0600

    fix(components): return to old version of the component

--- src/lib/components/TextAreaAutosize.svelte
+++ src/lib/components/TextAreaAutosize.svelte
@@ -9,36 +9,9 @@
 	export let focus_text: boolean = false;
 	//STYLES
 	export let placeHolder: string = "What's happening?";
-	export let maxSize: string = '';
-
-	let key: string;
+  export let maxSize: string = "";
 
 	$: outputText = innerText.replace(/\n{3,}/g, '\n\n');
-
-	function handleInput(event) {
-		key = event.key as string;
-		showPlaceHolder = false;
-		if (key === 'Backspace' && innerText.length === 0) showPlaceHolder = true;
-		//Evita que se cree un espacio al borrar el contenido
-		if (key === 'Backspace' && innerText.length === 1) {
-			innerText = '';
-			showPlaceHolder = true;
-		}
-		//Evita que se cree un espacio al borrar saltos de linea
-		if (
-			key === 'Backspace' &&
-			innerText.match(/(\r\n|\n|\r)/g)?.length === 3 &&
-			innerText.length == 3
-		) {
-			innerText = '';
-			showPlaceHolder = true;
-		}
-		//Evita que se agregue un espacio al ingresar espacios consecutivos
-		if (key === ' ') {
-      innerText = innerText.replace(/(\r\n|\n|\r)$/, 'a');
-			// showPlaceHolder = true;
-		}
-	}
 </script>
 
 <div class="relative">
@@ -47,9 +20,14 @@
 	{/if}
 	<div
 		class="text-bar text-white outline-none break-all relative overflow-y-auto max-h-80"
-		style="height: {maxSize};"
+    style="height: {maxSize};"
 		bind:innerText
-		on:keydown={handleInput}
+		on:input={() => {
+			showPlaceHolder =
+				innerText.trim().length === 0 && (innerText.match(/\n|\s/g) || []).length === 1
+					? true
+					: false;
+		}}
 		on:focus={() => {
 			focus = true;
 			focus_text = true;
