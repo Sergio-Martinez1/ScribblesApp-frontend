07454e38dbe7de279de05da76eb06bfaa04f305e
SergioD <sergio_david2109@hotmail.com>
2023-12-30 22:44:57 -0600

    chore(components): Formatting

---
+++ src/lib/components/Icon/ChevronRight.svelte
@@ -0,0 +1,8 @@
+
+<script lang="ts">
+  export let width: number = 24, height: number = 24;
+</script>
+
+<svg {width} {height} viewBox="0 0 95 96" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M35.625 72L59.375 48L35.625 24" stroke="white" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
--- src/lib/components/PostDetail.component.svelte
+++ src/lib/components/PostDetail.component.svelte
@@ -1,39 +1,27 @@
 <script lang="ts">
-	import UserIcon from '$lib/components/Icon/User.svelte';
-	import Reactions from '$lib/components/Reactions.component.svelte';
-	import PostOptions from '$lib/components/PostOptions.component.svelte';
-	import EditPost from '$lib/components/EditPost.componente.svelte';
-	import { calculate_posted_time } from '$lib/utils/calculate_posted_time';
 	import { enhance } from '$app/forms';
+	import { base } from '$app/paths';
 	import Image from './Icon/Image.svelte';
+	import X from './Icon/X.svelte';
+	import settings from '../../stores/postDetail';
+	import { afterNavigate } from '$app/navigation';
+
+  afterNavigate(({from})=>{
+    $settings.previousPage = from?.url.toString() || "";
+  })
+
+	let previous_page: string = base;
 
 	//POST
-	export let user_photo_url: string = '#';
-	export let user_name: string = 'user';
-	export let user_url: string = '#';
-
-	export let post_url: string = '#';
-	export let publication_date: string = '1d';
-	export let post_content: string = '';
 	export let post_thumbnail_url: string = '#';
 	export let loading: boolean = false;
 
 	// REACTIONS
-	let vertical: boolean = true;
-	export let like_on: boolean = false;
-	export let likes_count: number = 0;
-	export let comments_count: number = 0;
-	export let tags_count: number = 0;
-	export let tags: string[] = [];
-	export let post_by_tags_url: string = '/posts';
 	export let post_id: number = 0;
 
 	//POST OPTIONS
-	export let creator_id: number = 0;
-	export let myUser_id: number = -1;
 	let edit_dialog_id: string = `edit-dialog-post-${post_id}`;
 	let delete_dialog_id: string = `delete-dialog-post-${post_id}`;
-  let dont_show_dialog_id: string = `not_show_dialog_${post_id}`;
 
 	function handleClose() {
 		let element = document.getElementById(delete_dialog_id) as HTMLDialogElement;
@@ -45,7 +33,7 @@
 	}
 </script>
 
-<div class="bg-purpleGray rounded-2xl py-5">
+<div class="bg-purpleGray">
 	{#if loading}
 		<div class="animate-pulse flex flex-col px-4">
 			<div class="flex gap-4 mb-3">
@@ -60,53 +48,23 @@
 			<div class="w-full h-48 bg-purpleLight rounded-2xl" />
 		</div>
 	{:else}
-		<div class="flex items-center justify-between px-5">
-			<!-- Userphoto -->
-			<a href={user_url} class="w-[76px] h-[76px] rounded-full overflow-hidden">
-				{#if user_photo_url}
-					<img class="object-cover w-full h-full" src={user_photo_url} alt="User" />
-				{:else}
-					<UserIcon width={76} height={76} />
-				{/if}
-			</a>
-			<!-- Username -->
-			<div class=" h-fit ml-2.5">
-				<p class="px-3 py-1 text-white font-bold text-lg">
-					{user_name} · {calculate_posted_time(publication_date)}
-				</p>
-			</div>
-			<div class="grow" />
-			<!-- Reactions -->
-			<div class="mr-3">
-				<p class="text-white mx-auto w-fit text-sm mb-2">Post metadata</p>
-				<Reactions
-					{like_on}
-					{likes_count}
-					{comments_count}
-					{tags_count}
-					{post_url}
-					{vertical}
-					{tags}
-					{post_by_tags_url}
-					{post_id}
-				/>
-			</div>
-			<!-- PostOptions -->
-			<div class="mr-7">
-				<PostOptions {myUser_id} {creator_id} {delete_dialog_id} {edit_dialog_id} {dont_show_dialog_id} />
-			</div>
+		<div
+			class="grid grid-cols-[58px_minmax(0,_1fr)_1fr_30px] sm:grid-cols-[76px_minmax(0,_1fr)_1fr_30px] gap-x-3 items-center relative"
+		>
+			{#if $settings.previousPage.length}
+				<a
+					href={$settings.previousPage}
+					class="absolute top-4 left-4 bg-krispyPurple hover:bg-lessPurple active:bg-krispyPurple rounded-full w-9 h-9 flex items-center justify-center"
+					><X /></a
+				>
+			{/if}
 		</div>
-		<div class="flex px-9 mt-3">
+		<div class="flex">
 			<div class="w-full text-white">
-				<!-- Post content -->
-				<p class="mb-3 break-words">{post_content}</p>
-				<!-- Post thumbnail -->
 				{#if isValidImageUrl(post_thumbnail_url)}
-					<img
-						class="rounded-2xl overflow-hidden max-h-96 mx-auto"
-						src={post_thumbnail_url}
-						alt="Content"
-					/>
+					<div class="w-full bg-black h-screen flex items-center">
+						<img class="object-contain w-full h-full" src={post_thumbnail_url} alt="Content" />
+					</div>
 				{:else}
 					<div class="bg-purpleGray rounded-2xl p-6 w-fit mx-auto">
 						<div class="mx-auto w-fit mb-6">
@@ -154,15 +112,6 @@
 		<dialog
 			class="bg-purpleGray rounded-2xl w-[500px] shadow-[0px_0px_0px_1000px_rgba(18,21,23,0.7)] p-0"
 			id={edit_dialog_id}
-		>
-			<EditPost
-				{user_photo_url}
-				username={user_name}
-				{post_id}
-				innerText={post_content}
-				imagePreview={post_thumbnail_url}
-				{tags}
-			/>
-		</dialog>
+		/>
 	{/if}
 </div>
