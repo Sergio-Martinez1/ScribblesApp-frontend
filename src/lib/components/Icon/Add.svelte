f37d4b844aab82f520957ed54e8766c37c181d0e
SergioD <sergio_david2109@hotmail.com>
2024-04-09 19:17:51 -0600

    feat(Icons): Add new icons in /Icons

---
+++ src/lib/components/Icon/Add.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let width: number = 24, height: number = 24;
+</script>
+  
+<svg {width} {height} viewBox="0 0 102 102" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M32.234 51.0566H51.0567M51.0567 51.0566H69.8793M51.0567 51.0566V32.2339M51.0567 51.0566V69.8792" stroke="white" stroke-width="7.05849" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M51.0566 98.1132C77.045 98.1132 98.1132 77.045 98.1132 51.0566C98.1132 25.0679 77.045 4 51.0566 4C25.0679 4 4 25.0679 4 51.0566C4 77.045 25.0679 98.1132 51.0566 98.1132Z" stroke="white" stroke-width="7.05849" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
