1c2c1dbe0233fdbc4ed119bba841929bdd4b85db
SergioD <sergio_david2109@hotmail.com>
2024-04-10 22:29:10 -0600

    refactor(components): add EditPhoto component

--- src/lib/components/ProfilePhoto.component.svelte
+++ src/lib/components/ProfilePhoto.component.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import User from '$lib/components/Icon/User.svelte';
 	import EditPhoto from './EditPhoto.component.svelte';
+	import Camera from './Icon/Camera.svelte';
 
 	export let username: string = '';
 	export let profilePhoto: string = '';
@@ -9,18 +10,32 @@
 </script>
 
 {#if loading}
-	<div class="w-[174px] h-[174px] mb-2.5 relative bg-purpleDark box-content p-3 rounded-full">
-		<div class="animate-pulse duration-200 rounded-full bg-purpleLight w-[174px] h-[174px]" />
+	<div
+		class="w-w-[120px] h-[120px] sm:w-[190px] sm:h-[190px] mb-2.5 relative bg-purpleDark box-content p-3 rounded-full"
+	>
+		<div
+			class="animate-pulse duration-200 rounded-full bg-purpleLight w-[120px] h-[120px] sm:w-[190px] sm:h-[190px]"
+		/>
 	</div>
 	<div class="animate-pulse flex mx-auto w-fit h-fit">
 		<div class="h-6 w-36 bg-purpleLight rounded-2xl" />
 	</div>
 {:else if profilePhoto}
-	<div class="w-[100px] h-[100px] sm:w-[174px] sm:h-[174px] mb-2.5 relative bg-purpleDark box-content p-1.5 sm:p-3 rounded-full">
+	<div
+		class="w-[120px] h-[120px] sm:w-[190px] sm:h-[190px] mb-2.5 relative bg-purpleDark box-content p-1.5 sm:p-3 rounded-full"
+	>
 		<img class="w-full h-full object-cover rounded-full" src={profilePhoto} alt="User face" />
 		{#if editable}
-			<div class="absolute bottom-14 right-16 w-fit h-fit">
-				<EditPhoto />
+			<div
+				class="absolute bottom-0 sm:bottom-2 right-0 sm:right-8 w-fit h-fit"
+			>
+				<EditPhoto imagename={'profile_photo'} dialogId={'edit_profile_photo_dialog'}>
+					<div
+						class="w-fit h-fit p-2 rounded-full bg-krispyPurple hover:bg-lessPurple active:bg-krispyPurple"
+					>
+						<Camera />
+					</div>
+				</EditPhoto>
 			</div>
 		{/if}
 	</div>
@@ -30,7 +45,7 @@
 		<User width={174} height={174} />
 		{#if editable}
 			<div class="absolute bottom-14 right-16 w-fit h-fit">
-				<EditPhoto />
+				<EditPhoto imagename={'profile_photo'} dialogId={'edit_profile_photo_dialog_1'} />
 			</div>
 		{/if}
 	</div>
