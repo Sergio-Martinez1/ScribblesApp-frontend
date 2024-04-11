07e82fc3af5098eb866885b6f2a4ff42992c8cc0
SergioD <sergio_david2109@hotmail.com>
2024-04-10 22:34:01 -0600

    refactor(components): breaking changes in logic
    New logic for handling image crop and rezise.
    New styles and visualization for the images.
    The image cropped will be send to the api route.

--- src/lib/components/EditPhoto.component.svelte
+++ src/lib/components/EditPhoto.component.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import ArrowLeft from './Icon/ArrowLeft.svelte';
 	import Camera from '$lib/components/Icon/Camera.svelte';
-	import { applyAction, enhance } from '$app/forms';
-	import type { SubmitFunction } from '@sveltejs/kit';
 	import { invalidateAll } from '$app/navigation';
 
+	export let imagename: string = '';
+	export let dialogId: string = '';
+	export let cover_mode: boolean = false;
+
 	let uploadFile: HTMLInputElement;
-	//STATIC ELEMENT
-	export let widthA: number = 360;
-	export let heightA: number = 360;
 
 	//DINAMIC ELEMENT
 	export let widthB: number = 0;
@@ -25,10 +23,6 @@
 	let mouseOCoords = { x: 0, y: 0 };
 	let mouseNCoords = { x: 0, y: 0 };
 
-	//INNER
-	let fWidthA: string = widthA.toString() + 'px';
-	let fHeightA: string = heightA.toString() + 'px';
-
 	let fileUrl = '';
 	let image: HTMLImageElement;
 	let croppedUrl: string;
@@ -36,17 +30,21 @@
 	let container: HTMLDivElement;
 	let containerCoords: DOMRect;
 	let file: File | undefined;
-	let fileCropped: File | undefined;
-	let submitButton: HTMLButtonElement;
+	let fileCropped: File;
+	let divRed: HTMLDivElement;
+	let divPurple: HTMLDivElement;
 
-	onMount(() => {
-		document.addEventListener('mousemove', handleMouseMove);
-	});
+	let widthA: number;
+	let heightA: number;
+	const CoverWidth = 900;
+	const CoverHeight = 300;
+	const ProfileWidth = 340;
+	const ProfileHeight = 340;
+	let imageSizedWidth = widthB;
+	let imageSizedHeight = heightB;
 
 	function getCoords() {
 		containerCoords = container.getBoundingClientRect();
-		let divRed = document.querySelector('.divRed');
-		let divPurple = document.querySelector('.divPurple');
 
 		let coordsB = divRed?.getBoundingClientRect();
 		let coordsA = divPurple?.getBoundingClientRect();
@@ -109,7 +107,12 @@
 	}
 
 	function chargeFile(event: any) {
+		let element = document.getElementById(`${dialogId}`) as HTMLDialogElement;
+		element.showModal();
+		document.body.style.overflow = 'hidden';
+		document.addEventListener('mousemove', handleMouseMove);
 		containerCoords = container.getBoundingClientRect();
+
 		file = event.target.files?.[0];
 		if (!file) return;
 		fileUrl = URL.createObjectURL(file);
@@ -121,6 +124,9 @@
 				widthB = image.width;
 				heightB = image.height;
 				aspectRatio = Math.round((widthB / heightB) * 100) / 100;
+
+				widthA = divPurple.offsetWidth;
+				heightA = divPurple.offsetHeight;
 
 				//INITIAL SIZE
 				if (widthB != widthA) widthB = widthA;
@@ -139,76 +145,92 @@
 			};
 			if (typeof dataURL == 'string') {
 				image.src = dataURL;
-				let element = document.getElementById('edit_dialog_profile_photo') as HTMLDialogElement;
-				element.showModal();
 			}
 		};
 		reader.readAsDataURL(file);
 	}
 
-	function crop() {
+	async function crop() {
 		let canvas = document.createElement('canvas');
 		let context = canvas.getContext('2d');
-		let divRed = document.querySelector('.divRed');
 
-		imageSized = document.querySelector('.imageSized');
-		if (!image || !imageSized || !file) return;
-		canvas.width = imageSized.width;
-		canvas.height = imageSized.height;
+		if (!image || !imageSized) return;
 
-		context?.drawImage(imageSized, 0, 0, imageSized.width, imageSized.height);
+		let coords = getCoords();
+		let widthRatio = divRed.offsetWidth / divPurple.offsetWidth;
+		let heightRatio = divRed.offsetHeight / divPurple.offsetHeight;
+		let newWidth = (cover_mode ? CoverWidth : ProfileWidth) * widthRatio;
+		let newHeight = (cover_mode ? CoverHeight : ProfileHeight) * heightRatio;
+		let startX = ((coords.A.x - coords.B.x) / divRed.offsetWidth) * newWidth; // Posición inicial de corte en X
+		let startY = ((coords.A.y - coords.B.y) / divRed.offsetHeight) * newHeight; // Posición inicial de corte en Y
 
-		if (divRed) {
-			let coords = getCoords();
+		canvas.width = image.width;
+		canvas.height = image.height;
+		context?.drawImage(image, 0, 0, image.width, image.height, 0, 0, newWidth, newHeight);
 
-			let startX = coords.A.x - coords.B.x; // Posición inicial de corte en X
-			let startY = coords.A.y - coords.B.y; // Posición inicial de corte en Y
-			let croppedImage = context?.getImageData(startX, startY, widthA, heightA);
+		let croppedImage = context?.getImageData(
+			startX,
+			startY,
+			cover_mode ? CoverWidth : ProfileWidth,
+			cover_mode ? CoverHeight : ProfileHeight
+		);
 
-			if (croppedImage) {
-				let canvas2 = document.createElement('canvas');
-				let context2 = canvas2.getContext('2d');
-				canvas2.width = croppedImage.width;
-				canvas2.height = croppedImage.height;
-				context2?.putImageData(croppedImage, 0, 0);
-				canvas2.toBlob((blob) => {
-					if (!blob) return;
-					fileCropped = new File([blob], 'hola', { type: 'image/png' });
-				}, 'image/png');
-				croppedUrl = canvas2.toDataURL();
-				canvas2.remove();
-			}
+		if (croppedImage) {
+			let canvas2 = document.createElement('canvas');
+			let context2 = canvas2.getContext('2d');
+			canvas2.width = croppedImage.width;
+			canvas2.height = croppedImage.height;
+			context2?.putImageData(croppedImage, 0, 0);
+			canvas2.toBlob((blob) => {
+				if (!blob) return;
+				fileCropped = new File([blob], 'Imagen', { type: 'image/png' });
+			}, 'image/png');
+			croppedUrl = canvas2.toDataURL();
+			canvas2.remove();
+			document.removeEventListener('mousemove', handleMouseMove);
 		}
 		canvas.remove();
 	}
 
 	function handleClose() {
-		let element = document.getElementById('edit_dialog_profile_photo') as HTMLDialogElement;
+		let element = document.getElementById(`${dialogId}`) as HTMLDialogElement;
 		element.close();
+		document.body.style.overflow = 'auto';
+		croppedUrl = '';
+		if (imageSized) imageSized.src = '';
+		document.removeEventListener('mousemove', handleMouseMove);
+		console.log(imageSized);
 	}
 
-	const submit: SubmitFunction = () => {
-		return async ({ result }) => {
-			if (result.type === 'success') {
-				await invalidateAll();
-				await applyAction(result);
-				handleClose();
-			} else if (result.type === 'redirect') {
-				await applyAction(result);
-				handleClose();
-			}
-		};
+	const submit = async () => {
+		let formData = new FormData();
+		formData.append(`${imagename}`, fileCropped);
+
+		fetch('http://localhost:5173/api/upload', {
+			method: 'post',
+			body: formData
+		})
+			.then(async (response) => {
+				if (response.status == 200) {
+					await invalidateAll();
+				}
+			})
+			.catch((error) => {});
+
+		handleClose();
 	};
 </script>
 
 <button
 	type="button"
-	class="bg-krispyPurple hover:bg-lessPurple active:bg-krispyPurple rounded-full flex items-center justify-center p-2 absolute cursor-pointer"
+	class="flex items-center justify-center cursor-pointer"
 	on:click={() => {
 		uploadFile.click();
 	}}
 >
-	<div class="w-[24px] h-[24px]"><Camera /></div>
+	<slot />
+
+
 	<!-- IMAGE INPUT -->
 	<input
 		tabindex="-1"
@@ -222,70 +244,120 @@
 </button>
 
 <dialog
-	id="edit_dialog_profile_photo"
+	id={dialogId}
 	class="bg-purpleGray rounded-2xl shadow-[0px_0px_0px_1000px_rgba(18,21,23,0.7)] w-[600px] max-h-[600px] h-[90vh] py-3"
 >
-	{fileCropped}
-	<div class="grid grid-rows-[10%,1fr] h-full">
-		<form
-			method="POST"
-			enctype="multipart/form-data"
-			action="/profile?/editProfilePhoto"
-			use:enhance={submit}
-		>
-			<div class="grid grid-cols-[20%,1fr,20%] w-full h-fit mb-2 relative">
-				<button
-					on:click={handleClose}
-					class="col-span-1 w-fit hover:bg-purpleLight active:translate-y-[-1px] rounded-full p-1"
-					><ArrowLeft width={26} height={26} /></button
-				>
-				<p class="text-white font-bold text-xl col-span-1 w-fit justify-self-center">
-					Edit profile photo
-				</p>
+	<div id="edit_photo_form" class="grid grid-rows-[10%,90%] h-full">
+		<div class="grid grid-cols-[20%,1fr,20%] w-full h-fit mb-2 relative">
+			<button
+				type="button"
+				on:click={handleClose}
+				class="col-span-1 w-fit hover:bg-purpleLight active:translate-y-[-1px] rounded-full p-1"
+				><ArrowLeft width={26} height={26} /></button
+			>
+			<p class="text-white font-bold text-xl col-span-1 w-fit justify-self-center">
+				Edit profile photo
+			</p>
+			{#if !croppedUrl}
 				<button
 					on:click={crop}
 					type="button"
 					class="bg-krispyPurple hover:bg-lessPurple active:bg-krispyPurple rounded-full font-bold text-white px-2.5 py-1 col-span-1 w-fit justify-self-end"
 					>Apply</button
 				>
-				<button type="submit" tabindex="-1" bind:this={submitButton}>SUBMIT</button>
-				<input
-					type="hidden"
-					bind:value={fileCropped}
-					name="file_cropped"
-					on:change={() => {
-            console.log("TRUE")
-						submitButton.click();
-					}}
-				/>
-			</div>
-		</form>
+			{/if}
+		</div>
 
 		<div bind:this={container} class="w-full h-full">
-			<div
-				class="bg-gray-800 w-full h-full overflow-hidden relative flex"
-				on:mousedown={() => {
-					grab = true;
-				}}
-				on:mouseup={() => {
-					grab = false;
-				}}
-				on:mouseleave={() => {
-					grab = false;
-				}}
-				on:wheel={handleWheel}
-			>
-				<div
-					class="divPurple self-center mx-auto z-30 shadow-[0px_0px_0px_1000px_rgba(18,21,23,0.7)] border-krispyPurple border-[5px]"
-					style="width: {fWidthA}; height: {fHeightA};"
-				/>
-				<div
-					class="divRed text-white z-20 pointer-events-none absolute"
-					style="transform: translate({x}px, {y}px); width: {widthB}px; height: {heightB}px;"
-				>
-					<img class="imageSized w-full object-cover" src={image ? image.src : ''} alt="" />
+			{#if croppedUrl}
+				<div class="w-full h-full">
+					<div class="w-full h-[85%] flex justify-center items-center bg-purpleLight">
+						{#if !cover_mode}
+							<div class="aspect-square rounded-full h-[60%] sm:h-[80%]">
+								<img
+									class="w-full h-full object-cover rounded-full border-krispyPurple border-[5px]"
+									src={croppedUrl}
+									alt=""
+								/>
+							</div>
+						{:else}
+							<div class="w-[75%] aspect-[3/1] rounded-2xl">
+								<img
+									class="w-full h-full object-cover rounded-2xl border-krispyPurple border-[5px]"
+									src={croppedUrl}
+									alt=""
+								/>
+							</div>
+						{/if}
+					</div>
+					<div class="w-full h-[15%] flex items-center justify-center gap-x-14">
+						<button
+							on:click={handleClose}
+							class="bg-squeezeRed hover:bg-red-600 active:bg-squeezeRed rounded-full font-bold text-md text-white px-4 py-2 w-24"
+							type="button">Cancel</button
+						>
+						<button
+							on:click={submit}
+							class="bg-krispyPurple hover:bg-lessPurple active:bg-krispyPurple rounded-full font-bold text-md text-white px-4 py-2 w-24"
+							>Confirm</button
+						>
+					</div>
 				</div>
-			</div>
+			{:else}
+				<div
+					class="bg-gray-800 w-full h-full overflow-hidden relative flex {grab
+						? 'cursor-grabbing'
+						: 'cursor-grab'}"
+					on:mousedown={() => {
+						grab = true;
+					}}
+					on:mouseup={() => {
+						grab = false;
+					}}
+					on:mouseleave={() => {
+						grab = false;
+					}}
+					on:wheel={handleWheel}
+				>
+					{#if !cover_mode}
+						<div
+							bind:this={divPurple}
+							class="divPurple self-center mx-auto z-30 shadow-[0px_0px_0px_1000px_rgba(18,21,23,0.7)] border-krispyPurple border-[5px] rounded-full aspect-square h-[60%] sm:h-[80%]"
+						/>
+						<div
+							bind:this={divRed}
+							class="divRed text-white z-20 pointer-events-none absolute"
+							style="transform: translate({x}px, {y}px); width: {widthB}px; height: {heightB}px;"
+						>
+							<img
+								bind:this={imageSized}
+								class="imageSized w-full object-cover"
+								src={image ? image.src : ''}
+								alt=""
+							/>
+						</div>
+					{:else}
+						<div
+							bind:this={divPurple}
+							class="divPurple self-center mx-auto z-30 shadow-[0px_0px_0px_1000px_rgba(18,21,23,0.7)] border-krispyPurple border-[5px] rounded-2xl aspect-[3/1] w-[80%]"
+						/>
+						<div
+							bind:this={divRed}
+							class="divRed text-white z-20 pointer-events-none absolute"
+							style="transform: translate({x}px, {y}px); width: {widthB}px; height: {heightB}px;"
+						>
+							<img
+								bind:this={imageSized}
+								width={imageSizedWidth}
+								height={imageSizedHeight}
+								class="imageSized w-full object-cover"
+								src={image ? image.src : ''}
+								alt=""
+							/>
+						</div>
+					{/if}
+				</div>
+			{/if}
 		</div>
 	</div>
 </dialog>
