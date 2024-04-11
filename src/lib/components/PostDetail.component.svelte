02af697fa9f021c462862eb6fee0b787120f7759
SergioD <sergio_david2109@hotmail.com>
2024-04-10 22:38:24 -0600

    refactor(components): change styles, visualization

--- src/lib/components/PostDetail.component.svelte
+++ src/lib/components/PostDetail.component.svelte
@@ -1,20 +1,24 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import { base } from '$app/paths';
 	import Image from './Icon/Image.svelte';
 	import X from './Icon/X.svelte';
 	import settings from '../../stores/postDetail';
-	import { afterNavigate } from '$app/navigation';
+	import { afterNavigate, goto } from '$app/navigation';
+	import Logo from './Icon/Logo.svelte';
+	import { calculate_posted_time } from '$lib/utils/calculate_posted_time';
+	import User from './Icon/User.svelte';
 
-  afterNavigate(({from})=>{
-    $settings.previousPage = from?.url.toString() || "";
-  })
-
-	let previous_page: string = base;
+	afterNavigate(({ from }) => {
+		$settings.previousPage = from?.url.toString() || '';
+	});
 
 	//POST
 	export let post_thumbnail_url: string = '#';
+	export let post_content: string = '';
+	export let user_photo_url: string = '';
+	export let user_name: string = '';
 	export let loading: boolean = false;
+	export let post_creation_date: string = '';
 
 	// REACTIONS
 	export let post_id: number = 0;
@@ -31,6 +35,10 @@
 	function isValidImageUrl(url: string) {
 		return url.startsWith('http://') || url.startsWith('https://');
 	}
+
+	const navigateTo = (url: string) => {
+		goto('/home', { noScroll: true, invalidateAll: false });
+	};
 </script>
 
 <div class="bg-purpleGray">
@@ -48,19 +56,20 @@
 			<div class="w-full h-48 bg-purpleLight rounded-2xl" />
 		</div>
 	{:else}
-		<div
-			class="grid grid-cols-[58px_minmax(0,_1fr)_1fr_30px] sm:grid-cols-[76px_minmax(0,_1fr)_1fr_30px] gap-x-3 items-center relative"
-		>
+		<div class="absolute top-0 left-0 flex py-4 items-center z-10">
 			{#if $settings.previousPage.length}
-				<a
-					href={$settings.previousPage}
-					class="absolute top-4 left-4 bg-krispyPurple hover:bg-lessPurple active:bg-krispyPurple rounded-full w-9 h-9 flex items-center justify-center"
-					><X /></a
+				<button
+					on:click={() => {
+						navigateTo($settings.previousPage);
+					}}
+					class="bg-krispyPurple hover:bg-lessPurple active:bg-krispyPurple rounded-br-3xl rounded-tr-3xl w-12 h-12 flex items-center justify-center"
+					><X width={30} height={30} /></button
 				>
 			{/if}
+			<a href="/home" class="rounded-full ml-5"><Logo width={52} height={52} /></a>
 		</div>
-		<div class="flex">
-			<div class="w-full text-white">
+		<div class="flex h-screen relative">
+			<div class="w-full h-full text-white flex items-center">
 				{#if isValidImageUrl(post_thumbnail_url)}
 					<div class="w-full bg-black h-screen flex items-center">
 						<img class="object-contain w-full h-full" src={post_thumbnail_url} alt="Content" />
@@ -73,6 +82,24 @@
 						<p class="font-bold text-white">Image can't be loaded.</p>
 					</div>
 				{/if}
+			</div>
+			<div class="w-full h-fit absolute bottom-0 flex px-4 pt-5 pb-10">
+				<div class="bg-black w-full absolute top-0 left-0 h-full opacity-80 z-0" />
+				{#if user_photo_url}
+					<div class="w-16 h-16 mr-4 z-10">
+						<img src={user_photo_url} alt="" class="w-full h-full object-cover rounded-full" />
+					</div>
+				{:else}
+					<div class="mr-4 z-10">
+						<User width={64} height={64} />
+					</div>
+				{/if}
+				<div class="z-10 text-white">
+					<div>
+						<p class="font-bold">{user_name} · {calculate_posted_time(post_creation_date)}</p>
+					</div>
+					<p>{post_content}</p>
+				</div>
 			</div>
 		</div>
 
