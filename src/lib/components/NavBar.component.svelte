c28ac3b64a62626d3203f1ff3e424b8695b93560
SergioD <sergio_david2109@hotmail.com>
2024-04-10 22:24:12 -0600

    style(components): change styles

--- src/lib/components/NavBar.component.svelte
+++ src/lib/components/NavBar.component.svelte
@@ -12,43 +12,55 @@
 	export let settings_url: string = '/settings';
 	export let login_url: string = '/login';
 	export let logout_url: string = '/logout';
-  let icon_size = 60;
+	let icon_size = 60;
 </script>
 
 <nav
 	class="group bg-purpleGray p-3 sm:rounded-2xl w-full sm:w-[97px] lg:w-full transition-[width] overflow-hidden"
 >
-	<ul class="grid grid-flow-col sm:grid-flow-row max-sm:items-center h-fit max-sm:w-full sm:gap-y-2">
+	<ul
+		class="grid grid-flow-col sm:grid-flow-row max-sm:items-center h-fit max-sm:w-full sm:gap-y-2"
+	>
 		<li>
 			<a class:active={$page.url.pathname === home_url} href={home_url} title="Home"
-				><div class="w-[{icon_size}px] sm:w-[70px]"><Logo fill="#36254B" width={icon_size} height={icon_size} /></div>
-				<span class="hidden sm:inline-block opacity-0 lg:opacity-100">Home</span></a
+				><div class="w-[{icon_size}px]">
+					<Logo fill="#36254B" width={icon_size} height={icon_size} />
+				</div>
+				<span class="w-0">Home</span></a
 			>
 		</li>
 		{#if login}
 			<li>
 				<a class:active={$page.url.pathname === profile_url} href={profile_url} title="Profile"
-					><div class="w-[{icon_size}px] sm:w-[70px]"><User width={icon_size} height={icon_size} /></div>
-					<span class="hidden sm:inline-block opacity-0 lg:opacity-100">Profile</span></a
+					><div class="w-[{icon_size}px]">
+						<User width={icon_size} height={icon_size} />
+					</div>
+					<span>Profile</span></a
 				>
 			</li>
-		<li>
-			<a class:active={$page.url.pathname === settings_url} href={settings_url} title="Settings"
-				><div class="w-[{icon_size}px] sm:w-[70px]"><Settings width={icon_size} height={icon_size} /></div>
-				<span class="hidden sm:inline-block opacity-0 lg:opacity-100">Settings</span></a
-			>
-		</li>
+			<li>
+				<a class:active={$page.url.pathname === settings_url} href={settings_url} title="Settings"
+					><div class="w-[{icon_size}px]">
+						<Settings width={icon_size} height={icon_size} />
+					</div>
+					<span>Settings</span></a
+				>
+			</li>
 			<li>
 				<a class:active={$page.url.pathname === logout_url} href={logout_url}
-					><div class="w-[{icon_size}px] sm:w-[70px]"><LogOut width={icon_size} height={icon_size} /></div>
-					<span class="hidden sm:inline-block opacity-0 lg:opacity-100">Log Out</span></a
+					><div class="w-[{icon_size}px]">
+						<LogOut width={icon_size} height={icon_size} />
+					</div>
+					<span>Log Out</span></a
 				>
 			</li>
 		{:else}
 			<li>
 				<a class:active={$page.url.pathname === login_url} href={login_url}
-					><div class="w-[{icon_size}px] sm:w-[70px]"><LogIn width={icon_size} height={icon_size} /></div>
-					<span class="hidden sm:inline-block opacity-0 lg:opacity-100">Log In</span></a
+					><div class="w-[{icon_size}px]">
+						<LogIn width={icon_size} height={icon_size} />
+					</div>
+					<span>Log In</span></a
 				>
 			</li>
 		{/if}
@@ -57,10 +69,11 @@
 
 <style lang="postcss">
 	nav ul a {
-		@apply flex items-center hover:bg-hoverPurple rounded-2xl p-1.5;
+		@apply flex flex-col min-[1134px]:flex-row items-center hover:bg-hoverPurple rounded-2xl p-1.5;
 	}
 	nav ul li a span {
-		@apply font-bold text-white h-fit ml-3 whitespace-nowrap transition-opacity duration-300;
+		@apply font-bold text-white h-fit whitespace-nowrap transition-opacity duration-300;
+		@apply hidden sm:inline-block opacity-0 lg:opacity-100 w-0 h-0 lg:w-fit lg:h-fit min-[1134px]:pl-3;
 	}
 	nav ul li a.active span {
 		@apply text-krispyPurple;
