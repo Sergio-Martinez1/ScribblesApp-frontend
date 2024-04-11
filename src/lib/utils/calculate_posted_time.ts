425d864a0dc40b44283ba863b07915b5fe835032
SergioD <sergio_david2109@hotmail.com>
2024-04-10 22:40:10 -0600

    fix(utils): change logic for posted time

--- src/lib/utils/calculate_posted_time.ts
+++ src/lib/utils/calculate_posted_time.ts
@@ -1,31 +1,30 @@
 
 export function calculate_posted_time(publication_date: string) {
-  let time_now = new Date(Date.now());
-  let time_now_splited = time_now.toISOString().slice(0, 10).split('-').map(Number);
-  let publication_date_splited = publication_date.split('-').map(Number);
-  let output: number;
-  if (time_now_splited[0] - publication_date_splited[0] > 0) {
-    output = time_now_splited[0] - publication_date_splited[0];
-    return `${output}y`;
+  if (publication_date) {
+    let publication_date_parse = Date.parse(publication_date)
+    let time_now = Date.now()
+    let diference = time_now - publication_date_parse
+
+    diference = Math.floor(diference / 1000);
+    let output = `${diference} s`
+
+    if (diference > 60) {
+      diference = Math.floor(diference / 60);
+      output = `${diference} min`
+      if (diference > 60) {
+        diference = Math.floor(diference / 60);
+        output = `${diference} hrs`
+        if (diference > 24) {
+          diference = Math.floor(diference / 24);
+          output = `${diference} d`
+          if (diference > 365) {
+            diference = Math.floor(diference / 365)
+            output = `${diference} y`
+          }
+        }
+      }
+    }
+    return output;
   }
-  if (
-    (time_now_splited[1] * 30 +
-      time_now_splited[2]) -
-    (publication_date_splited[1] * 30 +
-      publication_date_splited[2]) >
-    90
-  ) {
-    output =
-      Math.round(((time_now_splited[1] * 30 +
-        time_now_splited[2]) -
-        (publication_date_splited[1] * 30 +
-          publication_date_splited[2])) / 30);
-    return `${output} months`;
-  }
-  output =
-    (time_now_splited[1] * 30 +
-      time_now_splited[2]) -
-    (publication_date_splited[1] * 30 +
-      publication_date_splited[2]);
-  return `${output}d`;
+  else return 'no date'
 }
