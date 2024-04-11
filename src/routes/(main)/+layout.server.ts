9c922ab7619bb345f1da86deb49cf10c471fc866
SergioD <sergio_david2109@hotmail.com>
2024-04-10 22:44:51 -0600

    fix(layout): add error handling

--- src/routes/(main)/+layout.server.ts
+++ src/routes/(main)/+layout.server.ts
@@ -4,7 +4,7 @@ import { error } from "@sveltejs/kit";
 
 export const load: LayoutServerLoad = async ({ cookies, fetch }) => {
   const base_api_url: string | undefined = env.API_URL;
-  if (!base_api_url) throw error(404, 'No api provided');
+  if (!base_api_url) console.error('Error: No se encontro la url de la api - [/routes/+layout.server.ts]');
 
   const access_token = cookies.get('access_token');
   if (access_token) {
@@ -17,11 +17,20 @@ export const load: LayoutServerLoad = async ({ cookies, fetch }) => {
         Authorization: `Bearer ${access_token}`
       }
     };
-    const my_reactions_response = await fetch(url, options);
-    if (my_reactions_response.ok) {
-      return {
-        my_reactions: my_reactions_response.json()
+    try {
+      const my_reactions_response = await fetch(url, options);
+      if (my_reactions_response.ok) {
+        return {
+          my_reactions: my_reactions_response.json()
+        }
+      } else {
+        throw error(my_reactions_response.status, my_reactions_response.statusText);
       }
+    } catch (error) {
+      console.error('Error: Error al intentar conectarse con la api - [/routes/(main)/+layout.server.ts]')
     }
+  }
+  return {
+    my_reactions: null
   }
 }
