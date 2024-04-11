daa6abbef470a27f1268c347ce8831adbe6d1a2a
SergioD <sergio_david2109@hotmail.com>
2024-04-10 22:45:41 -0600

    fix(layout): add error handling

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,30 +1,35 @@
 import type { LayoutServerLoad } from './$types';
 import { env } from '$env/dynamic/private';
-import { error, redirect } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 
 export const load: LayoutServerLoad = async ({ cookies, fetch }) => {
 
   const base_api_url: string | undefined = env.API_URL;
-  if (!base_api_url) throw error(404, 'No api provided');
-  
+  if (!base_api_url) console.error('Error: No se encontro la url de la api - [/routes/+layout.server.ts]');
+
   const access_token = cookies.get('access_token');
   if (!access_token) return { plainMyUser: null };
 
   const options = {
-    method: "GET",
+    method: "get",
     headers: {
       accept: "application/json",
       Authorization: `Bearer ${access_token}`
     }
   };
-  const response = await fetch(`${base_api_url}/users/plainMyUser`, options);
-  if (response.ok) {
-    return {
-      plainMyUser: response.json()
+  try {
+    const response = await fetch(`${base_api_url}/users/plainMyUser`, options);
+    if (response.ok) {
+      return {
+        plainMyUser: response.json()
+      }
+    } else {
+      throw error(response.status, response.statusText);
     }
-  } else {
-    return {
-      plainMyUser: null
-    };
-  } 
+  } catch (error) {
+    console.error('Error: Error al intentar conectarse con la api - [/routes/+layout.server.ts]')
+  }
+  return {
+    plainMyUser: null
+  };
 };
