15e9994d896153765781c7d67bbb916bcc7e3f96
SergioD <sergio_david2109@hotmail.com>
2024-04-10 22:47:38 -0600

    style(layout): change styles in main layout

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -9,16 +9,14 @@
 
 <div class="bg-purpleDark min-h-screen relative">
 	<div
-		class="grid sm:grid-cols-9 md:grid-cols-12 px-[20px] md:px-[20px] lg:px-[40px] sm:gap-[20px] max-w-7xl mx-auto relative"
+		class="grid grid-cols-7 sm:grid-cols-9 md:grid-cols-12 px-[20px] md:px-[20px] lg:px-[40px] sm:gap-[20px] max-w-7xl mx-auto relative"
 	>
-		<div class="hidden sm:inline-block sm:col-span-2 sm:mt-8 sm:relative">
-			<div class="fixed w-[174px]">
-				<NavBar login={isLogin} />
-			</div>
+		<div class="hidden sm:inline-block sm:col-span-2 top-[40px] sm:sticky h-fit">
+			<NavBar login={isLogin} />
 		</div>
 		<slot><!-- optional fallback --></slot>
 	</div>
 </div>
 <div class="bottom-0 fixed w-full sm:hidden">
-  <NavBar login={isLogin} />
+	<NavBar login={isLogin} />
 </div>
