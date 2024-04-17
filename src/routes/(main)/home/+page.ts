2562a22cac694b377f13df3719b2ad44bb58e3bf
SergioD <sergio_david2109@hotmail.com>
2024-04-16 19:58:42 -0600

    refactor(router): adjust returned data

--- src/routes/(main)/home/+page.ts
+++ src/routes/(main)/home/+page.ts
@@ -3,6 +3,6 @@ import type { PageLoad } from './$types';
 export const load: PageLoad = async ({ data }) => {
   
   return {
-    streamed: 'streamed' in data ? data.streamed : null
+    streamed: data.streamed
   };
 };
