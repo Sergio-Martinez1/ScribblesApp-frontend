424507e43c868fa9ab975ac78c9b6a81b4f6b82a
SergioD <sergio_david2109@hotmail.com>
2024-04-16 20:21:44 -0600

    refactor(router): styles, layout, errors

--- src/routes/(main)/profile/[id]/+page.svelte
+++ src/routes/(main)/profile/[id]/+page.svelte
@@ -7,7 +7,7 @@
 
 	import type { PageData } from './$types';
 	export let data: PageData;
-  $: plainMyUser = data.plainMyUser; 
+	$: plainMyUser = data.plainMyUser;
 	$: id = plainMyUser && plainMyUser.id ? Number(plainMyUser.id) : -1;
 	$: my_reactions = data.my_reactions;
 
@@ -22,68 +22,83 @@
 	}
 </script>
 
-{#await data.streamed?.user}
-	<div class="col-start-3 col-span-9 block mt-8">
-		<CoverPhoto loading={true} />
-	</div>
-	<div class="col-start-3 col-span-3">
-		<div class="relative h-full w-full">
-			<div class="absolute right-0 bottom-0">
-				<ProfilePhoto loading={true} />
+<div class="col-span-7 md:col-span-10 md:grid sm:col-start-3 md:gapx-[20px] md:grid-cols-10">
+	{#await data.streamed?.user}
+		<div class="col-span-10 lg:col-span-9 mt-8">
+			<CoverPhoto loading={true} />
+		</div>
+		<div
+			class="flex flex-col col-span-10 lg:grid grid-cols-7 md:grid-cols-10 my-4 lg:h-[160px] overflow-visible"
+		>
+			<div
+				class="md:col-span-4 lg:col-span-3 w-full h-[80px] sm:h-[160px] flex justify-center"
+			>
+				<div
+					class="w-full h-fit flex flex-col items-center justify-center self-end relative bottom-5 md:bottom-8"
+				>
+					<ProfilePhoto loading={true} />
+				</div>
+			</div>
+			<div class="md:col-span-6 flex items-center h-full">
+				<UserDescription loading={true} />
 			</div>
 		</div>
-	</div>
-	<div class="col-start-6 col-span-5">
-		<UserDescription loading={true} />
-	</div>
-{:then user}
-	<div class="sm:col-start-3 sm:col-span-9 block mt-8">
-		<CoverPhoto coverPhotoUrl={user.cover_photo} />
-	</div>
-	<div class="sm:col-start-3 sm:col-span-3 max-sm:mb-3">
-		<div class="relative h-[80px] sm:h-full w-full">
-			<div class="absolute max-sm:left-[50%] max-sm:translate-x-[-50%] sm:right-0 bottom-0">
-				<ProfilePhoto profilePhoto={user.profile_photo} username={user.username} />
-			</div>
+	{:then user}
+		<div class="col-span-10 lg:col-span-9 mt-8">
+			<CoverPhoto coverPhotoUrl={user.cover_photo} />
 		</div>
-	</div>
-	<div class="sm:col-start-6 sm:col-span-5 max-sm:mb-6">
-		<UserDescription
-			content={user.description}
-			webSite={user.personal_url}
-			birthDay={user.birthday}
-			location={user.location}
-			userCreationDate={user.creation_date}
-		/>
-	</div>
-{/await}
+		<div
+			class="flex flex-col col-span-10 lg:grid grid-cols-7 md:grid-cols-10 my-4 lg:h-[160px] overflow-visible"
+		>
+			<div class="md:col-span-4 lg:col-span-3 w-full h-[80px] sm:h-[160px] flex justify-center">
+				<div
+					class="w-full h-fit flex flex-col items-center justify-center self-end relative bottom-5 md:bottom-8"
+				>
+					<ProfilePhoto profilePhoto={user.profile_photo} username={user.username} />
+				</div>
+			</div>
 
-<div class="sm:col-start-4 sm:col-span-7 block mb-24 sm:mb-6">
-	<div class="flex flex-col gap-y-8">
-		{#await data.streamed?.posts}
-			<Post loading={true} />
-		{:then posts}
-			{#each posts as post}
-				<Post
-					user_photo_url={post.user.profile_photo}
-					user_name={post.user.username}
-					user_url="/profile/{post.user.id}"
-					post_url="/post/{post.id}"
-					publication_date={post.publication_date}
-          post_content={post.content ? post.content : ''}
-          post_thumbnail_url={post.thumbnail ? post.thumbnail : ''}
-					like_on={handleLike(my_reactions, post.id)}
-					likes_count={post.reactions.length}
-					comments_count={post.num_comments}
-					tags_count={post.tags.length}
-					vertical={false}
-					tags={post.tags.map((tag) => tag.content)}
-					post_by_tags_url="/home"
-					creator_id={post.user.id}
-					myUser_id={id}
-					post_id={post.id}
+			<div class="md:col-span-6 flex items-center h-full">
+				<UserDescription
+					content={user.description}
+					webSite={user.personal_url}
+					birthDay={user.birthday}
+					location={user.location}
+					userCreationDate={user.creation_date}
 				/>
-			{/each}
-		{/await}
+			</div>
+		</div>
+	{/await}
+
+	<div
+		class="col-start-1 col-span-9 md:col-start-1 lg:col-start-2 md:col-span-10 lg:col-span-7 block mb-28 sm:mb-6"
+	>
+		<div class="flex flex-col gap-y-8">
+			{#await data.streamed?.posts}
+				<Post loading={true} />
+			{:then posts}
+				{#each posts as post}
+					<Post
+						user_photo_url={post.user.profile_photo}
+						user_name={post.user.username}
+						user_url="/profile/{post.user.id}"
+						post_url="/post/{post.id}"
+						publication_date={post.publication_date}
+						post_content={post.content ? post.content : ''}
+						post_thumbnail_url={post.thumbnail ? post.thumbnail : ''}
+						like_on={handleLike(my_reactions, post.id)}
+						likes_count={post.reactions.length}
+						comments_count={post.num_comments}
+						tags_count={post.tags.length}
+						vertical={false}
+						tags={post.tags.map((tag) => tag.content)}
+						post_by_tags_url="/home"
+						creator_id={post.user.id}
+						myUser_id={id}
+						post_id={post.id}
+					/>
+				{/each}
+			{/await}
+		</div>
 	</div>
 </div>
