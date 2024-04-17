0f5e3d42b4541db6c8fcb702c6a82b695b594e64
SergioD <sergio_david2109@hotmail.com>
2024-04-16 20:30:12 -0600

    feat(components): added new components in settings

---
+++ src/lib/components/BirthdayInput.component.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import { slide } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+	import Plus from './Icon/Plus.svelte';
+	import Edit from './Icon/Edit.svelte';
+	import { tick } from 'svelte';
+
+	export let birthday: string = '';
+
+	let is_there_initial_content: boolean = birthday ? true : false;
+	let editable: boolean = false;
+	let content: string = birthday;
+	let input: HTMLInputElement;
+	$: validContent = birthday != content ? true : false;
+
+	function focusAndEnableInput() {
+		editable = true;
+		tick().then(() => {
+			input.disabled = false;
+			input.focus();
+		});
+	}
+</script>
+
+<form method="post" action="/settings?/editBirthday" use:enhance class="flex">
+	{#if !is_there_initial_content && !editable}
+		<button
+			type="button"
+			class="flex items-center text-white gap-x-2 bg-krispyPurple hover:bg-lessPurple active:bg-krispyPurple'} w-fit rounded-full py-1.5 px-1.5 mr-2"
+			on:click={focusAndEnableInput}
+		>
+			<div class="flex gap-x-2 items-center mx-1.5">
+				<Plus width={18} height={18} />
+				<p>Add Birthday</p>
+			</div>
+		</button>
+	{:else if is_there_initial_content || editable}
+		<input
+			bind:this={input}
+			bind:value={content}
+			class="bg-purpleLight rounded-2xl px-2 py-1 text-white outline-none focus:border-2 focus:border-krispyPurple box w-fit h-9 mr-2 disabled:opacity-50"
+			type="date"
+			id="birthday"
+			name="birthday"
+			placeholder="yyyy-mm-dd"
+			min="1900-01-01"
+			max="2030-12-31"
+			disabled
+		/>
+		{#if !editable}
+			<button
+				on:click={() => {
+					editable = true;
+					input.disabled = false;
+					input.focus();
+				}}><Edit color={'#931DF0'} /></button
+			>
+		{:else}
+			<button
+				type="button"
+				class="flex items-center text-white gap-x-2 bg-squeezeRed hover:bg-red-400 active:bg-squeezeRed w-fit rounded-full py-1.5 px-1.5 mr-2"
+				on:click={() => {
+					editable = false;
+					input.disabled = true;
+				}}
+			>
+				Cancel
+			</button>
+			<button
+				type="submit"
+				disabled={!validContent}
+				class="text-white bg-krispyPurple hover:bg-lessPurple active:bg-krispyPurple w-fit rounded-full py-1.5 px-1.5 disabled:bg-lessPurple disabled:opacity-[0.5]"
+				>Save</button
+			>
+            {content}
+		{/if}
+	{/if}
+</form>
---
+++ src/lib/components/LocationInput.component.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import { slide } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+	import Plus from './Icon/Plus.svelte';
+	import Edit from './Icon/Edit.svelte';
+	import { tick } from 'svelte';
+
+	export let location: string = '';
+
+	let is_there_initial_content: boolean = location ? true : false;
+	let editable: boolean = false;
+	let content: string = location;
+	let input: HTMLInputElement;
+	$: validContent = location != content ? true : false;
+
+	function focusAndEnableInput() {
+		editable = true;
+		tick().then(() => {
+			input.disabled = false;
+			input.focus();
+		});
+	}
+</script>
+
+<form
+	method="post"
+	action="/settings?/editLocation"
+	use:enhance
+	class="flex"
+>
+	{#if !is_there_initial_content && !editable}
+		<button
+			type="button"
+			class="flex items-center text-white gap-x-2 bg-krispyPurple hover:bg-lessPurple active:bg-krispyPurple'} w-fit rounded-full py-1.5 px-1.5 mr-2"
+			on:click={focusAndEnableInput}
+		>
+			<div class="flex gap-x-2 items-center mx-1.5">
+				<Plus width={18} height={18} />
+				<p>Add location</p>
+			</div>
+		</button>
+	{:else if is_there_initial_content || editable}
+		<input
+			bind:this={input}
+			type="text"
+			name="location"
+			bind:value={content}
+			class="bg-purpleLight rounded-2xl h-9 text-white mr-2 px-2 focus:outline-none focus:border-2 focus:border-krispyPurple disabled:opacity-50"
+			placeholder="New York, US"
+			disabled
+		/>
+		{#if !editable}
+			<button
+				on:click={() => {
+					editable = true;
+					input.disabled = false;
+					input.focus();
+				}}><Edit color={'#931DF0'} /></button
+			>
+		{:else}
+			<button
+				type="button"
+				class="flex items-center text-white gap-x-2 bg-squeezeRed hover:bg-red-400 active:bg-squeezeRed w-fit rounded-full py-1.5 px-1.5 mr-2"
+				on:click={() => {
+					editable = false;
+					input.disabled = true;
+				}}
+			>
+				Cancel
+			</button>
+			<button
+				type="submit"
+				disabled={!validContent}
+				class="text-white bg-krispyPurple hover:bg-lessPurple active:bg-krispyPurple w-fit rounded-full py-1.5 px-1.5 disabled:bg-lessPurple disabled:opacity-[0.5]"
+				>Save</button
+			>
+		{/if}
+	{/if}
+</form>
