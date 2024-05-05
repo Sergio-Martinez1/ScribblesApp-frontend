181dabbe2a3ccab6511d9cfed7ab122617156650
SergioD <sergio_david2109@hotmail.com>
2024-05-04 20:47:27 -0600

    refactor(page): update error handling and types

--- src/routes/(main)/home/+page.server.ts
+++ src/routes/(main)/home/+page.server.ts
@@ -2,15 +2,25 @@ import { type Actions, fail, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { env } from '$env/dynamic/private';
 import { error } from '@sveltejs/kit';
+import type { TopTag, Post } from '$lib/types';
 
 export const load: PageServerLoad = ({ fetch, cookies }) => {
-  let posts_response = null;
-  let top_tags_response = null;
-  let fetchedPosts = { data: null, status: 500 };
-  let fetchedTags = { data: null, status: 500 };
 
   const base_api_url: string | undefined = env.API_URL;
-  if (!base_api_url) console.error('Error: No se encontro la url de la api - [/routes/(main)/home/+layout.server.ts]');;
+  let fetchedPosts = { data: null, status: 500 };
+  let fetchedTags = { data: null, status: 500 };
+  let posts_response: Promise<{ data: Array<Post> | null, status: number }> = Promise.resolve(fetchedPosts);
+  let top_tags_response: Promise<{ data: Array<TopTag> | null, status: number }> = Promise.resolve(fetchedTags);
+
+  if (!base_api_url) {
+    console.error(`Error: Error en [/routes/(main)/home/+layout.server.ts].\n\t- No se encontro la url de la api en el entorno`);
+    return {
+      streamed: {
+        posts: posts_response,
+        top_tags: top_tags_response
+      }
+    }
+  }
 
   top_tags_response = fetch(`${base_api_url}/tags/top`)
     .then(async (response) => {
@@ -20,33 +30,13 @@ export const load: PageServerLoad = ({ fetch, cookies }) => {
         fetchedTags.data = data;
       }
       return fetchedTags;
-    }).catch(() => {
-      console.error('Error: Error al intentar conectarse con la api en ruta de tags - [/routes/(main)/home/+page.server.ts]')
+    }).catch((error) => {
+      console.error(`Error: Error en [/routes/(main)/home/+page.server.ts].\n\t- Error al intentar obtener "Top tags"\n\t- ${error}`)
+      return fetchedTags;
     })
 
   const access_token = cookies.get('access_token');
-  if (access_token) {
-    const home_url = `${base_api_url}/posts/home?offset=0&limit=20`;
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${access_token}`
-      }
-    };
-
-    posts_response = fetch(home_url, options)
-      .then(async (response) => {
-        fetchedPosts.status = response.status;
-        if (response.ok) {
-          const data = await response.json();
-          fetchedPosts.data = data;
-        }
-        return fetchedPosts;
-      }).catch(() => {
-        console.error('Error: Error al intentar conectarse con la api en ruta de posts - [/routes/(main)/home/+page.server.ts]')
-      })
-  } else {
+  if (!access_token) {
     posts_response = fetch(`${base_api_url}/posts/pagination?offset=0&limit=20`)
       .then(async (response) => {
         fetchedPosts.status = response.status;
@@ -55,10 +45,50 @@ export const load: PageServerLoad = ({ fetch, cookies }) => {
           fetchedPosts.data = data;
         }
         return fetchedPosts;
-      }).catch(() => {
-        console.error('Error: Error al intentar conectarse con la api en ruta de posts - [/routes/(main)/home/+page.server.ts]')
+      }).catch((error) => {
+        console.error(`Error: Error en [/routes/(main)/home/+page.server.ts].\n\t- Error al intentar obtener posts publicos\n\t- ${error}`)
+        return fetchedPosts;
       });
+    return {
+      streamed: {
+        posts: posts_response,
+        top_tags: top_tags_response
+      }
+    }
   }
+
+  const home_url = `${base_api_url}/posts/home?offset=0&limit=20`;
+  const posts_url = `${base_api_url}/posts/pagination?limit=20&offset=0`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${access_token}`
+    }
+  };
+
+  posts_response = fetch(home_url, options)
+    .then(async (response) => {
+      fetchedPosts.status = response.status;
+      if (response.ok) {
+        const data = await response.json();
+        fetchedPosts.data = data;
+      } else if (response.status == 401) {
+        await fetch(posts_url).then(async (response) => {
+          fetchedPosts.status = response.status;
+          if (response.ok) {
+            const data = await response.json();
+            fetchedPosts.data = data;
+          }
+        }).catch((error) => {
+          console.error(`Error: Error en [/routes/(main)/home/+page.server.ts].\n\t- Error al intentar obtener posts publicos\n\t- ${error}`)
+        })
+      }
+      return fetchedPosts;
+    }).catch((error) => {
+      console.error(`Error: Error en [/routes/(main)/home/+page.server.ts].\n\t- Error al intentar obtener posts de home\n\t- ${error}`)
+      return fetchedPosts;
+    })
 
   return {
     streamed: {
@@ -82,6 +112,10 @@ export const actions: Actions = {
     const access_token = cookies.get('access_token');
     if (!access_token) throw redirect(303, '/login');
     const base_api_url: string = env.API_URL;
+    if (!base_api_url) {
+      console.error(`Error(Action): Error en [/routes/(main)/home/+layout.server.ts].\n\t- No se encontro la url de la api en el entorno`);
+      return fail(500, { serverFail: true });
+    }
     if (state_on === 'true') {
       const url = `${base_api_url}/reactions/`;
       const body = {
@@ -101,9 +135,9 @@ export const actions: Actions = {
         const response = await fetch(url, options);
         if (response.ok) return;
         else if (response.status === 401) throw redirect(303, "/login");
-        return fail(400, { badRequest: true });
+        return fail(500, { serverFail: true });
       } catch (error) {
-        console.error('Error: Error al intentar conectarse con la api - [/routes/(main)/home/+page.server.ts]')
+        console.error(`Error(Action): Error en [/routes/(main)/home/+page.server.ts].\n\t- Error al intentar toogle_reaction\n\t- ${error}`)
         return fail(500, { serverFail: true });
       }
 
@@ -121,12 +155,12 @@ export const actions: Actions = {
         const response = await fetch(url, options);
         if (response.ok) return;
         else if (response.status === 401) throw redirect(303, "/login");
-        return fail(400, { badRequest: true });
+        return fail(500, { serverFail: true });
       } catch (error) {
-        console.error('Error: Error al intentar conectarse con la api - [/routes/(main)/home/+page.server.ts]')
+        console.error(`Error(Action): Error en [/routes/(main)/home/+page.server.ts].\n\t- Error al intentar toogle_reaction\n\t- ${error}`)
         return fail(500, { serverFail: true });
       }
-      
+
     }
   },
   createPost: async ({ request, fetch, cookies }) => {
@@ -136,12 +170,16 @@ export const actions: Actions = {
     const raw_tags = form.get('tags')?.toString();
     const tags = raw_tags ? raw_tags?.split(',') : '';
     const is_there_thumbnail = thumbnail?.size > 0 ? true : false;
-
     const base_api_url: string = env.API_URL;
     const posts_url = `${base_api_url}/posts/`;
     const files_url = `${base_api_url}/files/`;
     const access_token = cookies.get('access_token');
+
     if (!access_token) throw redirect(303, '/login');
+    if (!base_api_url) {
+      console.error(`Error(Action): Error en [/routes/(main)/home/+layout.server.ts].\n\t- No se encontro la url de la api en el entorno`);
+      return fail(500, { serverFail: true });
+    }
 
     let image_url_request = async (file: File) => {
       const formData = new FormData();
@@ -153,11 +191,17 @@ export const actions: Actions = {
         }),
         body: formData
       };
-      const response = await fetch(files_url, options);
-      if (response.ok) {
-        let img = await response.json();
-        return img.url;
-      } else return '';
+      try {
+        const response = await fetch(files_url, options);
+        if (response.ok) {
+          let img = await response.json();
+          return img.url;
+        }
+        throw error(response.status, response.statusText);
+      } catch (error) {
+        console.error(`Error(Action): Error en [/routes/(main)/home/+page.server.ts].\n\t- Error al obtener url de la imagen\n\t- ${error}`)
+        return ''
+      }
     };
 
     let thumbnail_url: string = '';
@@ -180,17 +224,27 @@ export const actions: Actions = {
       },
       body: JSON.stringify(body)
     };
-    const response = await fetch(posts_url, options);
-    if (response.ok) return;
-    else if (response.status === 401) throw redirect(303, "/login");
+    try {
+      const response = await fetch(posts_url, options);
+      if (response.ok) return;
+      else if (response.status === 401) throw redirect(303, "/login");
+      throw error(response.status, response.statusText);
+    } catch (error) {
+      console.error(`Error(Action): Error en [/routes/(main)/home/+page.server.ts].\n\t- Error al intentar crear un post\n\t- ${error}`)
+      return fail(500, { serverFail: true });
+    }
   },
   deletePost: async ({ request, fetch, cookies }) => {
     const form = await request.formData();
     const post_id = form.get('post_id');
     const base_api_url: string | undefined = env.API_URL;
     const access_token = cookies.get('access_token');
+
     if (!access_token) throw redirect(303, '/login');
-    if (!base_api_url) throw error(404, 'No api provided');
+    if (!base_api_url) {
+      console.error(`Error(Action): Error en [/routes/(main)/home/+layout.server.ts].\n\t- No se encontro la url de la api en el entorno`);
+      return fail(500, { serverFail: true });
+    }
     if (!post_id) return fail(400, { postIdMissing: true });
 
     const posts_url = `${base_api_url}/posts/${post_id}`;
@@ -201,9 +255,15 @@ export const actions: Actions = {
         Authorization: `Bearer ${access_token}`
       }
     };
-    const response = await fetch(posts_url, options);
-    if (response.ok) return;
-    else if (response.status === 401) throw redirect(303, "/login");
+    try {
+      const response = await fetch(posts_url, options);
+      if (response.ok) return;
+      else if (response.status === 401) throw redirect(303, "/login");
+      throw error(response.status, response.statusText);
+    } catch (error) {
+      console.error(`Error(Action): Error en [/routes/(main)/home/+page.server.ts].\n\t- Error al intentar eliminar un post\n\t- ${error}`)
+      return fail(500, { serverFail: true });
+    }
   },
   editPost: async ({ request, fetch, cookies }) => {
     const form = await request.formData();
@@ -214,13 +274,17 @@ export const actions: Actions = {
     const raw_tags = form.get('tags')?.toString();
     const tags = raw_tags ? raw_tags?.split(',') : '';
     const is_there_thumbnail = thumbnail?.size > 0 ? true : false;
-
-    if (!post_id) return fail(400, { postIdMissing: true });
-
     const base_api_url: string = env.API_URL;
     const post_url = `${base_api_url}/posts/${post_id}`;
     const files_url = `${base_api_url}/files/`;
     const access_token = cookies.get('access_token');
+
+    if (!access_token) throw redirect(303, '/login');
+    if (!base_api_url) {
+      console.error(`Error(Action): Error en [/routes/(main)/home/+layout.server.ts].\n\t- No se encontro la url de la api en el entorno`);
+      return fail(500, { serverFail: true });
+    }
+    if (!post_id) return fail(400, { postIdMissing: true });
 
     let image_url_request = async (file: File) => {
       const formData = new FormData();
@@ -232,11 +296,17 @@ export const actions: Actions = {
         }),
         body: formData
       };
-      const response = await fetch(files_url, options);
-      if (response.ok) {
-        let img = await response.json();
-        return img.url;
-      } else if (response.status === 401) throw redirect(303, "/login");
+      try {
+        const response = await fetch(files_url, options);
+        if (response.ok) {
+          let img = await response.json();
+          return img.url;
+        } else if (response.status === 401) throw redirect(303, "/login");
+        throw error(response.status, response.statusText);
+      } catch (error) {
+        console.error(`Error(Action): Error en [/routes/(main)/home/+page.server.ts].\n\t- Error al intentar obtener la url de la imagen\n\t- ${error}`)
+        return fail(500, { serverFail: true });
+      }
     };
 
     let thumbnail_url: string = '';
@@ -262,9 +332,15 @@ export const actions: Actions = {
       },
       body: JSON.stringify(body)
     };
-    const response = await fetch(post_url, options);
-    if (response.ok) return;
-    else if (response.status === 401) throw redirect(303, "/login");
+    try {
+      const response = await fetch(post_url, options);
+      if (response.ok) return;
+      else if (response.status === 401) throw redirect(303, "/login");
+      throw error(response.status, response.statusText);
+    } catch (error) {
+      console.error(`Error(Action): Error en [/routes/(main)/home/+page.server.ts].\n\t- Error al intentar editar el post \n\t- ${error}`)
+      return fail(500, { serverFail: true });
+    }
   },
   dontShowPost: async ({ request, fetch, cookies }) => {
     const form = await request.formData();
@@ -272,7 +348,10 @@ export const actions: Actions = {
     const base_api_url: string | undefined = env.API_URL;
     const access_token = cookies.get('access_token');
     if (!access_token) throw redirect(303, '/login');
-    if (!base_api_url) throw error(404, 'No api provided');
+    if (!base_api_url) {
+      console.error(`Error(Action): Error en [/routes/(main)/home/+layout.server.ts].\n\t- No se encontro la url de la api en el entorno`);
+      return fail(500, { serverFail: true });
+    }
     if (!post_id) return fail(400, { postIdMissing: true });
 
     const posts_url = `${base_api_url}/posts/dont_show_post/${post_id}`;
@@ -283,8 +362,14 @@ export const actions: Actions = {
         Authorization: `Bearer ${access_token}`
       }
     };
-    const response = await fetch(posts_url, options);
-    if (response.ok) return;
-    else if (response.status === 401) throw redirect(303, "/login");
+    try {
+      const response = await fetch(posts_url, options);
+      if (response.ok) return;
+      else if (response.status === 401) throw redirect(303, "/login");
+      throw error(response.status, response.statusText);
+    } catch (error) {
+      console.error(`Error(Action): Error en [/routes/(main)/home/+page.server.ts].\n\t- Error al intentar ocultar un post \n\t- ${error}`)
+      return fail(500, { serverFail: true });
+    }
   }
 };
