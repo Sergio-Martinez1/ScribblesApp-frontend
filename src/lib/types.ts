675be91a1b8117fc2bb92aa70f42539c0bdc504d
SergioD <sergio_david2109@hotmail.com>
2024-05-06 20:12:46 -0600

    feat(utils): add new types for data in pages

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,3 +2,18 @@ export interface TopTag {
   content: string
   count: number
 }
+export interface Post {
+  id: number,
+  content: string,
+  thumbnail: string,
+  publication_date: string,
+  user_id: number,
+  user: {
+    id: number,
+    username: string,
+    profile_photo: string,
+  }
+  reactions: Array<{ id: number, user_id: number, post_id: number }>,
+  tags: Array<{ content: string, post_id: number }>,
+  num_comments: number
+}
