7138b8c150deb1dd4c3432fb74856ea2e496cd85
SergioD <sergio_david2109@hotmail.com>
2024-05-08 16:19:26 -0600

    feat(types): add new types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,16 +4,50 @@ export interface TopTag {
 }
 export interface Post {
   id: number,
-  content: string,
-  thumbnail: string,
+  content: string | null,
+  thumbnail: string | null,
   publication_date: string,
   user_id: number,
   user: {
     id: number,
     username: string,
-    profile_photo: string,
+    profile_photo: string | null,
   }
-  reactions: Array<{ id: number, user_id: number, post_id: number }>,
-  tags: Array<{ content: string, post_id: number }>,
-  num_comments: number
+  reactions: Array<{ id: number, user_id: number, post_id: number }> | null,
+  tags: Array<{ content: string, post_id: number }> | null,
+  num_comments: number | null
+}
+export interface Comment {
+  id: number,
+  content: string,
+  creation_date: string,
+  post_id: number,
+  user_id: number,
+  user: {
+    id: number,
+    username: string,
+    profile_photo: string | null
+  }
+}
+export interface MyUser {
+  username: string,
+  email: string,
+  creation_date: string,
+  profile_photo: string | null,
+  cover_photo: string | null,
+  description: string | null,
+  personal_url: string | null,
+  location: string | null,
+  birthday: string | null,
+  prohibited_posts: Array<number> | null
+}
+export interface User {
+  username: string,
+  creation_date: string,
+  profile_photo: string | null,
+  cover_photo: string | null,
+  description: string | null,
+  personal_url: string | null,
+  location: string | null,
+  birthday: string | null,
 }
