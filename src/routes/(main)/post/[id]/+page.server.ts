b30ba850fb080ca7edad25dcfc155fc97825b3ab
SergioD <sergio_david2109@hotmail.com>
2024-05-08 13:07:11 -0600

    refactor(page): break changes
    Changes in retrieved data, now include "data" and "status".
    Changes in error display, add a custom error message.

--- src/routes/(main)/post/[id]/+page.server.ts
+++ src/routes/(main)/post/[id]/+page.server.ts
@@ -1,27 +1,58 @@
 import type { PageServerLoad } from "./$types";
 import { env } from "$env/dynamic/private";
-import { error, type Actions, redirect, fail } from "@sveltejs/kit";
+import { type Actions, redirect, fail } from "@sveltejs/kit";
+import type { Post, Comment } from '$lib/types';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __route = __filename.slice(__filename.indexOf('src'));
 
 export const load: PageServerLoad = async ({ fetch, params }) => {
   const base_api_url: string | undefined = env.API_URL;
-  if (!base_api_url) throw error(404, 'No api provided');
+  let fetchedPost = { data: null, status: 500 };
+  let fetchedComments = { data: null, status: 500 };
+  let post_response: Promise<{ data: Post | null, status: number }> = Promise.resolve(fetchedPost);
+  let comments_response: Promise<{ data: Array<Comment> | null, status: number }> = Promise.resolve(fetchedComments);
 
-  let data: { post: any; comments: any; } = {
-    post: null,
-    comments: null
+  if (!base_api_url) {
+    console.error(`Error: Error en [${__route}].\n\t- No se encontro la url de la api en el entorno`);
+    return {
+      streamed: {
+        posts: post_response,
+        comments: comments_response
+      }
+    }
   }
-  const post_response = await fetch(`${base_api_url}/posts/${params.id}`);
-  if (post_response.ok) {
-    data.post = post_response.json()
-  }
-  const comments_response = await fetch(`${base_api_url}/comments/${params.id}`);
-  if (comments_response.ok) {
-    data.comments = comments_response.json()
-  }
+
+  post_response = fetch(`${base_api_url}/posts/${params.id}`)
+    .then(async (response) => {
+      fetchedPost.status = response.status;
+      if (response.ok) {
+        const data = await response.json();
+        fetchedPost.data = data;
+      }
+      return fetchedPost;
+    }).catch((error) => {
+      console.error(`Error: Error en [${__route}].\n\t- Error al intentar obtener la inf del post\n\t- ${error}`)
+      return fetchedPost;
+    });
+  comments_response = fetch(`${base_api_url}/comments/${params.id}`)
+    .then(async (response) => {
+      fetchedComments.status = response.status;
+      if (response.ok) {
+        const data = await response.json();
+        fetchedComments.data = data;
+      }
+      return fetchedComments;
+    }).catch((error) => {
+      console.error(`Error: Error en [${__route}].\n\t- Error al intentar obtener los comentarios del post\n\t- ${error}`)
+      return fetchedPost;
+    });
+
   return {
     streamed: {
-      post: data.post,
-      comments: data.comments ? data.comments : [],
+      post: post_response,
+      comments: comments_response,
     }
   }
 };
@@ -33,8 +64,15 @@ export const actions: Actions = {
     const post_id = form.get('post_id');
     const base_api_url: string | undefined = env.API_URL;
     const access_token = cookies.get('access_token');
+
     if (!access_token) throw redirect(303, "/login");
-    if (!base_api_url) throw error(404, 'No api provided');
+    if (!base_api_url) {
+      console.error(`Error(Action): Error en [${__route}].\n\t- No se encontro la url de la api en el entorno`);
+      return fail(500, { serverFail: true });
+    }
+    if (!content) return fail(400, { contentMissing: true });
+    if (!post_id) return fail(400, { post_idMissing: true });
+
     const url = `${base_api_url}/comments/`;
     const body = {
       "content": content?.toString(),
@@ -49,17 +87,27 @@ export const actions: Actions = {
       },
       body: JSON.stringify(body)
     }
-    const response = await fetch(url, options);
-    if (response.ok) return;
-    else if (response.status === 401) throw redirect(303, "/login");
+    try {
+      const response = await fetch(url, options);
+      if (response.ok) return;
+      else if (response.status !== 401) return fail(response.status, { serverFail: true });
+    } catch (error) {
+      console.error(`Error(Action): Error en [${__route}].\n\t- Error al intentar createComment\n\t- ${error}`)
+      return fail(500, { serverFail: true });
+    }
+    throw redirect(303, "/login");
   },
   deleteComment: async ({ request, fetch, cookies }) => {
     const form = await request.formData();
     const comment_id = form.get('comment_id');
     const base_api_url: string | undefined = env.API_URL;
     const access_token = cookies.get('access_token');
+
     if (!access_token) throw redirect(303, "/login");
-    if (!base_api_url) throw error(404, 'No api provided');
+    if (!base_api_url) {
+      console.error(`Error(Action): Error en [${__route}].\n\t- No se encontro la url de la api en el entorno`);
+      return fail(500, { serverFail: true });
+    }
     if (!comment_id) return fail(400, { commentIdMissing: true });
 
     const url = `${base_api_url}/comments/${comment_id}`;
@@ -70,17 +118,27 @@ export const actions: Actions = {
         Authorization: `Bearer ${access_token}`
       }
     }
-    const response = await fetch(url, options);
-    if (response.ok) return;
-    else if (response.status === 401) throw redirect(303, "/login");
+    try {
+      const response = await fetch(url, options);
+      if (response.ok) return;
+      else if (response.status !== 401) return fail(response.status, { serverFail: true });
+    } catch (error) {
+      console.error(`Error(Action): Error en [${__route}].\n\t- Error al intentar deleteComment\n\t- ${error}`)
+      return fail(500, { serverFail: true });
+    }
+    throw redirect(303, "/login");
   },
-  deletePost: async ({ request, fetch, cookies, url }) => {
+  deletePost: async ({ request, fetch, cookies }) => {
     const form = await request.formData();
     const post_id = form.get('post_id');
     const base_api_url: string | undefined = env.API_URL;
     const access_token = cookies.get('access_token');
+
     if (!access_token) throw redirect(303, "/login");
-    if (!base_api_url) throw error(404, 'No api provided');
+    if (!base_api_url) {
+      console.error(`Error(Action): Error en [${__route}].\n\t- No se encontro la url de la api en el entorno`);
+      return fail(500, { serverFail: true });
+    }
     if (!post_id) return fail(400, { postIdMissing: true });
 
     const posts_url = `${base_api_url}/posts/${post_id}`;
@@ -91,9 +149,14 @@ export const actions: Actions = {
         Authorization: `Bearer ${access_token}`
       }
     }
-    const response = await fetch(posts_url, options);
-    if (response.ok && url) {
-      throw redirect(303, "/home");
-    } else if (response.status === 401) throw redirect(303, "/login");
+    try {
+      const response = await fetch(posts_url, options);
+      if (response.ok) throw redirect(303, "/home");
+      else if (response.status !== 401) return fail(response.status, { serverFail: true });
+    } catch (error) {
+      console.error(`Error(Action): Error en [${__route}].\n\t- Error al intentar deletePost\n\t- ${error}`)
+      return fail(500, { serverFail: true });
+    }
+    throw redirect(303, "/login");
   }
 }
