c1325b5e46688fdd1a023892e893e3d65858fe35
SergioD <sergio_david2109@hotmail.com>
2024-05-08 13:18:02 -0600

    fix(page): fix error display

--- src/routes/(main)/profile/[id]/+page.server.ts
+++ src/routes/(main)/profile/[id]/+page.server.ts
@@ -1,19 +1,57 @@
-import type { PageServerLoad } from "./$types";
 import { env } from '$env/dynamic/private';
-import { error } from "@sveltejs/kit";
+import { fileURLToPath } from 'url';
+import type { Post, User } from "$lib/types";
+import type { PageServerLoad } from "./$types";
+
+const __filename = fileURLToPath(import.meta.url);
+const __route = __filename.slice(__filename.indexOf('src'));
 
 export const load: PageServerLoad = async ({ fetch, params }) => {
   const base_api_url: string | undefined = env.API_URL;
-  if (!base_api_url) throw error(404, 'No api provided');
+  let fetchedUser = { data: null, status: 500 };
+  let fetchedUserPosts = { data: null, status: 500 };
+  let user_response: Promise<{ data: User | null, status: number }> = Promise.resolve(fetchedUser);
+  let user_posts_response: Promise<{ data: Array<Post> | null, status: number }> = Promise.resolve(fetchedUserPosts);
 
-  const user_response = await fetch(`${base_api_url}/users/${params.id}`);
-  const user_posts_response = await fetch(`${base_api_url}/posts/user/${params.id}`);
-  if (user_response.ok && user_posts_response.ok) {
+  if (!base_api_url) {
+    console.error(`Error: Error en [${__route}].\n\t- No se encontro la url de la api en el entorno`);
     return {
       streamed: {
-        user: user_response.json(),
-        posts: user_posts_response.json()
+        myUser: user_response,
+        myPosts: user_posts_response
       }
-    };
+    }
   }
+
+  user_response = fetch(`${base_api_url}/users/${params.id}`)
+    .then(async (response) => {
+      fetchedUser.status = response.status;
+      if (response.ok) {
+        const data = await response.json();
+        fetchedUser.data = data;
+      }
+      return fetchedUser;
+    }).catch((error)=>{
+      console.error(`Error: Error en [${__route}].\n\t- Error al intentar obtener "user"\n\t- ${error}`)
+      return fetchedUser;
+    });
+  user_posts_response = fetch(`${base_api_url}/posts/user/${params.id}`)
+    .then(async (response) => {
+      fetchedUserPosts.status = response.status;
+      if (response.ok) {
+        const data = await response.json();
+        fetchedUserPosts.data = data;
+      }
+      return fetchedUserPosts;
+    }).catch((error)=>{
+      console.error(`Error: Error en [${__route}].\n\t- Error al intentar obtener "user_posts"\n\t- ${error}`)
+      return fetchedUserPosts;
+    });
+
+  return {
+    streamed: {
+      user: user_response,
+      posts: user_posts_response
+    }
+  };
 };
