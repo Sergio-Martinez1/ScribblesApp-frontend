1689093e171b3639a4a18dfeddfd07e8fafb6500
SergioD <sergio_david2109@hotmail.com>
2024-05-08 11:43:28 -0600

    fix(auth): fix error handling and display

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -1,42 +1,62 @@
-import { fail, type Actions, redirect } from '@sveltejs/kit';
+import { fail, type Actions, redirect, error } from '@sveltejs/kit';
 import { env } from '$env/dynamic/private';
+import { fileURLToPath } from 'url';
 
 export const actions: Actions = {
-	login: async ({ request, fetch, cookies }) => {
-		const form = await request.formData();
-		const username = form.get('username');
-		const password = form.get('password');
-		if (!username) {
-			return fail(400, { usernameMissing: true });
-		}
-		if (!password) {
-			return fail(400, { passwordMissing: true, username });
-		}
+  login: async ({ request, fetch, cookies }) => {
 
-		const base_api_url: string = env.API_URL;
-		const url = `${base_api_url}/users/signin`;
-		const body = new URLSearchParams();
-		body.append('username', username.toString());
-		body.append('password', password.toString());
+    const __filename = fileURLToPath(import.meta.url);
+    const __route = __filename.slice(__filename.indexOf('src'));
 
-		const options = {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded'
-			},
-			body: body.toString()
-		};
-		const response = await fetch(url, options);
-		if (response.ok) {
-			const responseJSON = await response.json();
-      cookies.set('access_token', responseJSON.access_token, {
-        path: '/',
-      });
-      throw redirect(303, "/home");
-    } else if (response.status === 404) {
-        return fail(400, { usernameDontExists: true});
-    } else if (response.status === 400) {
-        return fail(400, { wrongPassword: true, username });
+    const form = await request.formData();
+    const username = form.get('username');
+    const password = form.get('password');
+
+    if (!username) {
+      return fail(400, { usernameMissing: true });
     }
-	}
+    if (!password) {
+      return fail(400, { passwordMissing: true, username });
+    }
+
+    const base_api_url: string = env.API_URL;
+    if (!base_api_url) {
+      console.error(`Error: Error en [${__route}].\n\t- No se encontro la url de la api en el entorno`);
+      return fail(500, { serverFail: true });
+    }
+
+    const url = `${base_api_url}/users/signin`;
+    const body = new URLSearchParams;
+    body.append('username', username.toString());
+    body.append('password', password.toString());
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: body.toString()
+    };
+
+    try {
+      const response = await fetch(url, options);
+      if (response.ok) {
+        const responseJSON = await response.json();
+        cookies.set('access_token', responseJSON.access_token, {
+          path: '/',
+        });
+      } else if (response.status === 404) {
+        return fail(400, { usernameDontExists: true });
+      } else if (response.status === 400) {
+        return fail(400, { wrongPassword: true, username });
+      } else {
+        throw error(response.status, response.statusText);
+      }
+    } catch (error) {
+      console.error(`Error: Error en [${__route}].\n\t- Error en el servidor al intentar iniciar sesion\n\t- ${error}`);
+      return fail(500, { serverFail: true });
+    }
+
+    throw redirect(303, "/home");
+  }
 };
