a7a51fc7643716b1548a2eb67be458094d10c1a4
SergioD <sergio_david2109@hotmail.com>
2024-05-08 11:44:34 -0600

    chore(page): clean code

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { PageData, ActionData } from './$types';
+	import type { ActionData } from './$types';
 	import { Login } from '$components';
 	export let form: ActionData;
 </script>
