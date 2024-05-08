4a5b18cf1a0248bc01f0a3fc419118471db59616
SergioD <sergio_david2109@hotmail.com>
2024-05-08 13:08:43 -0600

    refactor(page): adjust page to the new data format

--- src/routes/(main)/post/[id]/+page@.svelte
+++ src/routes/(main)/post/[id]/+page@.svelte
@@ -3,6 +3,8 @@
 	import Comments from '$components/Comments.component.svelte';
 	import type { PageData } from './$types';
 	import { page } from '$app/stores';
+  
+  //FALTA IMPLEMENTAR LAS REACCIONES
 
 	export let data: PageData;
 	$: plainMyUser = data.plainMyUser;
@@ -24,21 +26,29 @@
 </script>
 
 <div class="bg-purpleDark min-h-screen relative">
-			<div
-		class="grid grid-cols-9 lg:grid-cols-12 lg:pr-[40px] lg:gap-[20px] mx-auto relative"
-	>
+	<div class="grid grid-cols-9 lg:grid-cols-12 lg:pr-[40px] lg:gap-[20px] mx-auto relative">
 		<div class="col-span-9 h-screen lg:sticky lg:top-0">
 			{#await data.streamed?.post}
 				<PostDetail loading={true} />
 			{:then post}
-				<PostDetail
-					post_thumbnail_url={post.thumbnail ? post.thumbnail : ''}
-					post_content={post.content}
-          post_creation_date={post.publication_date}
-					user_photo_url={post.user.profile_photo}
-          user_name={post.user.username}
-					post_id={post.id}
-				/>
+				{#if post?.status === 200 && post.data}
+					<PostDetail
+						post_thumbnail_url={post.data.thumbnail ? post.data.thumbnail : ''}
+						post_content={post.data.content}
+						post_creation_date={post.data.publication_date}
+						user_photo_url={post.data.user.profile_photo}
+						user_name={post.data.user.username}
+						post_id={post.data.id}
+					/>
+				{:else if post?.status === 404}
+					<div class="bg-purpleGray rounded-2xl flex justify-center p-3">
+						<p class="text-white">Post Not found</p>
+					</div>
+				{:else}
+					<div class="bg-purpleGray rounded-2xl flex justify-center p-3">
+						<p class="text-white">Please reload the page</p>
+					</div>
+				{/if}
 			{/await}
 		</div>
 
@@ -46,13 +56,23 @@
 			{#await data.streamed?.comments}
 				<Comments loading={true} />
 			{:then comments}
-				<Comments
-					{comments}
-					{isLogin}
-					my_user_photo={profile_photo}
-					post_id={Number($page.params.id)}
-					my_user_id={id}
-				/>
+				{#if comments?.status === 200 && comments.data}
+					<Comments
+            comments={comments.data}
+						{isLogin}
+						my_user_photo={profile_photo}
+						post_id={Number($page.params.id)}
+						my_user_id={id}
+					/>
+				{:else if comments?.status === 404}
+					<div class="bg-purpleGray rounded-2xl flex justify-center p-3">
+						<p class="text-white">Comments Not found</p>
+					</div>
+				{:else}
+					<div class="bg-purpleGray rounded-2xl flex justify-center p-3">
+						<p class="text-white">Please reload the page</p>
+					</div>
+				{/if}
 			{/await}
 		</div>
 	</div>
