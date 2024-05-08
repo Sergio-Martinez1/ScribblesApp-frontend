c51573a84c1583e44417b5d42ddfcb7a59d9d7b0
SergioD <sergio_david2109@hotmail.com>
2024-05-08 11:58:47 -0600

    fix(page): fix error display

--- src/routes/(auth)/signup/+page.server.ts
+++ src/routes/(auth)/signup/+page.server.ts
@@ -1,13 +1,19 @@
 import { fail, type Actions, redirect } from '@sveltejs/kit';
 import { env } from '$env/dynamic/private';
+import { fileURLToPath } from 'url';
 
 export const actions: Actions = {
   signup: async ({ request, fetch }) => {
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __route = __filename.slice(__filename.indexOf('src'));
+
     const form = await request.formData();
     const username = form.get('username');
     const email = form.get('email');
     const password = form.get('password');
     const confirm = form.get('confirm');
+
     if (!username) {
       return fail(400, { usernameMissing: true, email });
     }
@@ -25,6 +31,11 @@ export const actions: Actions = {
     }
 
     const base_api_url: string = env.API_URL;
+    if (!base_api_url) {
+      console.error(`Error: Error en [${__route}].\n\t- No se encontro la url de la api en el entorno`);
+      return fail(500, { serverFail: true });
+    }
+
     const url = `${base_api_url}/users/signup`;
 
     const body = {
@@ -39,17 +50,24 @@ export const actions: Actions = {
       },
       body: JSON.stringify(body)
     };
-    const response = await fetch(url, options);
-    if (response.ok) {
-      throw redirect(303, '/login');
-    } else if (response.status === 400) {
-      const res = await response.json();
-      const detail = res.detail;
-      if (detail === 'Username already exist'){
-        return fail(400, { usernameExists: true, email });
-      } else {
-        return fail(400, { emailExists: true, username });
+
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        if (response.status === 400) {
+          const res = await response.json();
+          const detail = res.detail;
+          if (detail === 'Username already exist') {
+            return fail(400, { usernameExists: true, email });
+          } else {
+            return fail(400, { emailExists: true, username });
+          }
+        }
       }
+    } catch (error) {
+      console.error(`Error: Error en [${__route}].\n\t- Error en el servidor al intentar registrar usuario\n\t- ${error}`);
+      return fail(500, { serverFail: true });
     }
+    throw redirect(303, '/login');
   }
 };
