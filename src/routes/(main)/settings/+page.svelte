4a51b5fc4f28b66e97e32d69cf7650f55a000b59
SergioD <sergio_david2109@hotmail.com>
2024-05-13 17:06:03 -0600

    feat(page): update dark_mode component

--- src/routes/(main)/settings/+page.svelte
+++ src/routes/(main)/settings/+page.svelte
@@ -6,6 +6,7 @@
 	import { fly } from 'svelte/transition';
 	import ArrowLeft from '$components/Icon/ArrowLeft.svelte';
 	import { onMount } from 'svelte';
+	import DarkModeSelector from '$components/DarkModeSelector.component.svelte';
 
 	export let form: ActionData;
 	export let data: PageData;
@@ -14,7 +15,7 @@
 	let screenWidth: number;
 	let password_view: boolean = false;
 	let username_view: boolean = false;
-	let toggle_theme_view: boolean = false;
+	let dark_mode_view: boolean = false;
 	let delete_account_view: boolean = false;
 	let open_view: boolean = false;
 	let screenScroll: number = 0;
@@ -37,7 +38,7 @@
 		edit_profile_view = false;
 		password_view = false;
 		username_view = false;
-		toggle_theme_view = false;
+		dark_mode_view = false;
 		delete_account_view = false;
 	}
 </script>
@@ -48,105 +49,125 @@
 	bind:scrollY={screenScroll}
 />
 <div
-	class="col-span-4 flex flex-col items-center gap-y-4 bg-purpleGray p-6 rounded-2xl sm:my-8 h-fit mt-4 mb-28 sticky top-8"
+	class="col-span-4 flex flex-col items-center gap-y-4 bg-lavandaGray dark:bg-purpleGray p-6 rounded-2xl sm:my-8 h-fit mt-4 mb-28 sticky top-8"
 >
-	<h1 class="text-white font-bold text-3xl">Settings</h1>
+	<h1 class="dark:text-white font-bold text-3xl">Settings</h1>
 	<button
 		class="{edit_profile_view
-			? 'bg-krispyPurple cursor-auto'
-			: 'bg-purpleLight cursor-pointer hover:bg-hoverPurple active:bg-purpleLight'} border-b-krispyPurple min-w-full text-center rounded-2xl py-3"
+			? 'bg-krispyPurple text-white cursor-auto'
+			: 'bg-lavandaLight dark:bg-purpleLight cursor-pointer hover:bg-hoverLavanda dark:hover:bg-hoverPurple active:bg-lavandaLight dark:active:bg-purpleLight'} border-b-krispyPurple min-w-full text-center rounded-2xl py-3"
 		on:click={() => {
 			open_view = true;
 			edit_profile_view = true;
 			username_view = false;
 			password_view = false;
-			toggle_theme_view = false;
+			dark_mode_view = false;
 			delete_account_view = false;
 		}}
 	>
-		<h1 class="text-white text-lg max-sm:flex max-sm:items-center max-sm:relative max-sm:h-[30px]">
+		<h1
+			class="dark:text-white text-lg max-sm:flex max-sm:items-center max-sm:relative max-sm:h-[30px]"
+		>
 			<p class="max-sm:absolute max-sm:left-[50%] max-sm:translate-x-[-50%]">Edit Profile</p>
-			<div class="w-fit h-fit sm:hidden max-sm:absolute max-sm:right-5"><ChevronRight /></div>
+			<div class="w-fit h-fit sm:hidden max-sm:absolute max-sm:right-5">
+				<ChevronRight tailwindStrokeClass={'stroke-black dark:stroke-white'} />
+			</div>
 		</h1>
 	</button>
 	<button
 		class="{username_view
-			? 'bg-krispyPurple cursor-auto'
-			: 'bg-purpleLight cursor-pointer hover:bg-hoverPurple active:bg-purpleLight'} min-w-full text-center rounded-2xl py-3"
+			? 'bg-krispyPurple text-white cursor-auto'
+			: 'bg-lavandaLight dark:bg-purpleLight cursor-pointer hover:bg-hoverLavanda dark:hover:bg-hoverPurple active:bg-lavandaLight dark:active:bg-purpleLight'} min-w-full text-center rounded-2xl py-3"
 		on:click={() => {
 			open_view = true;
 			edit_profile_view = false;
 			username_view = true;
 			password_view = false;
-			toggle_theme_view = false;
+			dark_mode_view = false;
 			delete_account_view = false;
 			screenScroll = 0;
 		}}
 	>
-		<h1 class="text-white text-lg max-sm:flex max-sm:items-center max-sm:relative max-sm:h-[30px]">
+		<h1
+			class="dark:text-white text-lg max-sm:flex max-sm:items-center max-sm:relative max-sm:h-[30px]"
+		>
 			<p class="max-sm:absolute max-sm:left-[50%] max-sm:translate-x-[-50%]">
 				Change your username
 			</p>
-			<div class="w-fit h-fit sm:hidden max-sm:absolute max-sm:right-5"><ChevronRight /></div>
+			<div class="w-fit h-fit sm:hidden max-sm:absolute max-sm:right-5">
+				<ChevronRight tailwindStrokeClass={'stroke-black dark:stroke-white'} />
+			</div>
 		</h1>
 	</button>
 	<button
 		class="{password_view
-			? 'bg-krispyPurple cursor-auto'
-			: 'bg-purpleLight cursor-pointer hover:bg-hoverPurple active:bg-purpleLight'} min-w-full text-center rounded-2xl py-3"
+			? 'bg-krispyPurple text-white cursor-auto'
+			: 'bg-lavandaLight dark:bg-purpleLight cursor-pointer hover:bg-hoverLavanda dark:hover:bg-hoverPurple active:bg-lavandaLight dark:active:bg-purpleLight'} min-w-full text-center rounded-2xl py-3"
 		on:click={() => {
 			open_view = true;
 			edit_profile_view = false;
 			username_view = false;
 			password_view = true;
-			toggle_theme_view = false;
+			dark_mode_view = false;
 			delete_account_view = false;
 			screenScroll = 0;
 		}}
 	>
-		<h1 class="text-white text-lg max-sm:flex max-sm:items-center max-sm:relative max-sm:h-[30px]">
+		<h1
+			class="dark:text-white text-lg max-sm:flex max-sm:items-center max-sm:relative max-sm:h-[30px]"
+		>
 			<p class="max-sm:absolute max-sm:left-[50%] max-sm:translate-x-[-50%]">
 				Change your password
 			</p>
-			<div class="w-fit h-fit sm:hidden max-sm:absolute max-sm:right-5"><ChevronRight /></div>
+			<div class="w-fit h-fit sm:hidden max-sm:absolute max-sm:right-5">
+				<ChevronRight tailwindStrokeClass={'stroke-black dark:stroke-white'} />
+			</div>
 		</h1>
 	</button>
 	<button
-		class="{toggle_theme_view
-			? 'bg-krispyPurple cursor-auto'
-			: 'bg-purpleLight cursor-pointer hover:bg-hoverPurple active:bg-purpleLight'} min-w-full text-center rounded-2xl py-3"
+		class="{dark_mode_view
+			? 'bg-krispyPurple text-white cursor-auto'
+			: 'bg-lavandaLight dark:bg-purpleLight cursor-pointer hover:bg-hoverLavanda dark:hover:bg-hoverPurple active:bg-lavandaLight dark:active:bg-purpleLight'} min-w-full text-center rounded-2xl py-3"
 		on:click={() => {
 			open_view = true;
 			edit_profile_view = false;
 			username_view = false;
 			password_view = false;
-			toggle_theme_view = true;
+			dark_mode_view = true;
 			delete_account_view = false;
 			screenScroll = 0;
 		}}
 	>
-		<h1 class="text-white text-lg max-sm:flex max-sm:items-center max-sm:relative max-sm:h-[30px]">
-			<p class="max-sm:absolute max-sm:left-[50%] max-sm:translate-x-[-50%]">Toggle theme</p>
-			<div class="w-fit h-fit sm:hidden max-sm:absolute max-sm:right-5"><ChevronRight /></div>
+		<h1
+			class="dark:text-white text-lg max-sm:flex max-sm:items-center max-sm:relative max-sm:h-[30px]"
+		>
+			<p class="max-sm:absolute max-sm:left-[50%] max-sm:translate-x-[-50%]">Dark mode</p>
+			<div class="w-fit h-fit sm:hidden max-sm:absolute max-sm:right-5">
+				<ChevronRight tailwindStrokeClass={'stroke-black dark:stroke-white'} />
+			</div>
 		</h1>
 	</button>
 	<button
 		class="{delete_account_view
-			? 'bg-krispyPurple cursor-auto'
-			: 'bg-purpleLight cursor-pointer hover:bg-hoverPurple active:bg-purpleLight'} min-w-full text-center rounded-2xl py-3"
+			? 'bg-krispyPurple text-white cursor-auto'
+			: 'bg-lavandaLight dark:bg-purpleLight cursor-pointer hover:bg-hoverLavanda dark:hover:bg-hoverPurple active:bg-lavandaLight dark:active:bg-purpleLight'} min-w-full text-center rounded-2xl py-3"
 		on:click={() => {
 			open_view = true;
 			edit_profile_view = false;
 			username_view = false;
 			password_view = false;
-			toggle_theme_view = false;
+			dark_mode_view = false;
 			delete_account_view = true;
 			screenScroll = 0;
 		}}
 	>
-		<h1 class="text-white text-lg max-sm:flex max-sm:items-center max-sm:relative max-sm:h-[30px]">
+		<h1
+			class="dark:text-white text-lg max-sm:flex max-sm:items-center max-sm:relative max-sm:h-[30px]"
+		>
 			<p class="max-sm:absolute max-sm:left-[50%] max-sm:translate-x-[-50%]">Delete your account</p>
-			<div class="w-fit h-fit sm:hidden max-sm:absolute max-sm:right-5"><ChevronRight /></div>
+			<div class="w-fit h-fit sm:hidden max-sm:absolute max-sm:right-5">
+				<ChevronRight tailwindStrokeClass={'stroke-black dark:stroke-white'} />
+			</div>
 		</h1>
 	</button>
 </div>
@@ -159,12 +180,19 @@
 				<div
 					in:fly={{ x: window.innerWidth, easing: quintOut, opacity: 1 }}
 					out:fly|local={{ x: window.innerWidth, easing: quintOut, opacity: 1 }}
-					class="max-sm:absolute max-sm:w-full sm:inline-block sm:col-start-7 sm:col-span-6 px-5 py-4 min-h-screen max-sm:bg-purpleDark"
+					class="max-sm:absolute max-sm:w-full sm:inline-block sm:col-start-7 sm:col-span-6 px-5 py-4 min-h-screen max-sm:bg-lavandaDark dark:max-sm:bg-purpleGray"
 				>
-					<div class="flex flex-col items-center gap-y-4 bg-purpleGray rounded-2xl h-fit mb-24 p-3">
+					<div
+						class="flex flex-col items-center gap-y-4 bg-lavandaGray dark:bg-purpleGray rounded-2xl h-fit mb-24 p-3"
+					>
 						<button
 							class="self-start active:bg-krispyPurple sm:hidden rounded-full"
-							on:click={hideView}><ArrowLeft width={40} height={40} /></button
+							on:click={hideView}
+							><ArrowLeft
+								width={40}
+								height={40}
+								tailwindStrokeClass={'stroke-black dark:stroke-white'}
+							/></button
 						>
 						{#if edit_profile_view}
 							<EditProfile
@@ -176,18 +204,22 @@
 								birthday={user.data.birthday}
 							/>
 						{:else if username_view}
-							<ChangeUsername {form} username={user.data.username} />
+							<ChangeUsername username={user.data.username} />
 						{:else if password_view}
 							<ChangePassword {form} />
-						{:else if toggle_theme_view}
+						{:else if dark_mode_view}
 							<label class="flex items-center relative w-max cursor-pointer select-none py-9">
-								<span class="text-2xl font-bold mr-3 text-white">Dark Theme</span>
+								<span class="text-2xl font-bold mr-3 dark:text-white">Dark Theme</span>
 								<input
 									type="checkbox"
 									class="appearance-none transition-colors cursor-pointer w-14 h-7 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-black focus:ring-blue-500 bg-red-500"
 								/>
-								<span class="absolute font-medium text-xs uppercase right-1 text-white"> OFF </span>
-								<span class="absolute font-medium text-xs uppercase right-8 text-white"> ON </span>
+								<span class="absolute font-medium text-xs uppercase right-1 dark:text-white">
+									OFF
+								</span>
+								<span class="absolute font-medium text-xs uppercase right-8 dark:text-white">
+									ON
+								</span>
 								<span
 									class="w-7 h-7 right-7 absolute rounded-full transform transition-transform bg-gray-200"
 								/>
@@ -200,7 +232,7 @@
 			{/if}
 		{:else}
 			<div class="inline-block col-start-7 col-span-6 min-h-screen pt-8 pb-16">
-				<div class="bg-purpleGray rounded-2xl h-fit">
+				<div class="bg-lavandaGray dark:bg-purpleGray rounded-2xl h-fit p-6">
 					{#if edit_profile_view}
 						<EditProfile
 							profile_photo={user.data.profile_photo}
@@ -214,19 +246,8 @@
 						<ChangeUsername {form} username={user.data.username} />
 					{:else if password_view}
 						<ChangePassword {form} />
-					{:else if toggle_theme_view}
-						<label class="flex items-center relative w-max cursor-pointer select-none py-9">
-							<span class="text-2xl font-bold mr-3 text-white">Dark Theme</span>
-							<input
-								type="checkbox"
-								class="appearance-none transition-colors cursor-pointer w-14 h-7 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-black focus:ring-blue-500 bg-red-500"
-							/>
-							<span class="absolute font-medium text-xs uppercase right-1 text-white"> OFF </span>
-							<span class="absolute font-medium text-xs uppercase right-8 text-white"> ON </span>
-							<span
-								class="w-7 h-7 right-7 absolute rounded-full transform transition-transform bg-gray-200"
-							/>
-						</label>
+					{:else if dark_mode_view}
+						<DarkModeSelector />
 					{:else if delete_account_view}
 						<DeleteAccount />
 					{/if}
