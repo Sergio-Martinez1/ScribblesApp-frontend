24627c45695b70c4f16fb46fffe5f70972be3d7a
SergioD <sergio_david2109@hotmail.com>
2024-05-13 16:53:00 -0600

    feat(config): add new colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,17 +9,25 @@ module.exports = {
     },
     extend: {
       colors: {
-        purpleLight: '#36254B',
-        purpleGray: '#281B38',
         purpleDark: '#1F152B',
-        krispyPurple: '#931DF0',
-        jacketBlue: '#2C41FF',
+        purpleGray: '#281B38',
+        purpleLight: '#36254B',
         lessPurple: '#7F18D0',
         hoverPurple: '#483164',
-        squeezeRed: '#F0431D'
+
+        lavandaDark: '#e8e1ed',
+        lavandaGray: '#fff',
+        lavandaLight: '#e3dfe6',
+        lessLavanda: '#c091e6',
+        hoverLavanda: '#d4c1e3',
+
+        krispyPurple: '#931DF0',
+        jacketBlue: '#2C41FF',
+        squeezeRed: '#F0431D',
       },
     }
   },
+  darkMode: 'class',
   plugins: []
 };
 
