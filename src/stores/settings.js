efcb1c4ee8469a4c10d91ec1b9a0c0a88deb63e1
SergioD <sergio_david2109@hotmail.com>
2024-05-17 19:07:23 -0600

    refactor(stores): add new prop in settings

--- src/stores/settings.js
+++ src/stores/settings.js
@@ -3,7 +3,8 @@ import { writable } from "svelte/store";
 const settings = writable({
   previousPage: "",
   dark_mode: "disabled",
-  color_scheme: ""
+  color_scheme: "",
+  scrollY: 0
 });
 
 export default settings;
