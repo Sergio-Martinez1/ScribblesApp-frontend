bda16e8f7b5299177358cffc013ff74c2289e13c
SergioD <sergio_david2109@hotmail.com>
2024-05-17 19:08:03 -0600

    style(page): update styles in error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -8,7 +8,7 @@
 	$: isLogin = myUser ? true : false;
 </script>
 
-<div class="bg-purpleDark min-h-screen relative">
+<div class="bg-lavandaDark dark:bg-purpleDark min-h-screen relative">
 	<div
 		class="grid grid-cols-7 sm:grid-cols-9 md:grid-cols-12 px-[20px] md:px-[20px] lg:px-[40px] sm:gap-[20px] max-w-7xl mx-auto relative min-h-screen"
 	>
@@ -17,8 +17,8 @@
 				<NavBar login={isLogin} />
 			</div>
 		</div>
-		<div class="col-span-7 md:col-span-10 text-white p-4 flex flex-col justify-center min-h-screen">
-			<div class="flex flex-col items-center justify-center bg-purpleGray rounded-2xl p-20 pt-2">
+		<div class="col-span-7 md:col-span-10 dark:text-white p-4 flex flex-col justify-center min-h-screen">
+			<div class="flex flex-col items-center justify-center bg-lavandaGray dark:bg-purpleGray rounded-2xl p-20 pt-2">
 				{#if $page.status === 404}
 					<p id="oops" class="font-bold text-[100px] md:text-[200px]">Oops!</p>
 					<p class="md:text-[35px] text-center">
