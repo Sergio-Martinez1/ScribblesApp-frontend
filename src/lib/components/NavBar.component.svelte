4d6cd07859cfcee15747e89bf9e41f26c632d60a
SergioD <sergio_david2109@hotmail.com>
2024-05-25 09:38:56 -0600

    styles: update icon size logic

--- src/lib/components/NavBar.component.svelte
+++ src/lib/components/NavBar.component.svelte
@@ -12,7 +12,6 @@
 	export let settings_url: string = '/settings';
 	export let login_url: string = '/login';
 	export let logout_url: string = '/logout';
-	let icon_size = 60;
 </script>
 
 <nav
@@ -22,70 +21,50 @@
 		class="grid grid-flow-col sm:grid-flow-row max-sm:items-center h-fit max-sm:w-full sm:gap-y-2"
 	>
 		<li>
-			<a class:active={$page.url.pathname === home_url} href={home_url} title="Home"
-				><div class="w-[{icon_size}px]">
-					<Logo
-						fill="#36254B"
-						width={icon_size}
-						height={icon_size}
-						tailwindFillClass={'fill-lavandaLight dark:fill-purpleLight'}
-						tailwindStrokeClass={'stroke-black dark:stroke-white'}
-					/>
-				</div>
+			<a class:active={$page.url.pathname === home_url} href={home_url} title="Home">
+				<Logo
+					fill="#36254B"
+					tailwindFillClass={'fill-lavandaLight dark:fill-purpleLight'}
+					tailwindStrokeClass={'stroke-black dark:stroke-white'}
+				/>
 				<span class="w-0">Home</span></a
 			>
 		</li>
 		{#if login}
 			<li>
-				<a class:active={$page.url.pathname === profile_url} href={profile_url} title="Profile"
-					><div class="w-[{icon_size}px]">
-						<User
-							width={icon_size}
-							height={icon_size}
-							tailwindFillClass={'fill-lavandaLight dark:fill-purpleLight'}
-							tailwindStrokeClass={'stroke-black dark:stroke-white'}
-						/>
-					</div>
+				<a class:active={$page.url.pathname === profile_url} href={profile_url} title="Profile">
+					<User
+						tailwindFillClass={'fill-lavandaLight dark:fill-purpleLight'}
+						tailwindStrokeClass={'stroke-black dark:stroke-white'}
+					/>
 					<span>Profile</span></a
 				>
 			</li>
 			<li>
-				<a class:active={$page.url.pathname === settings_url} href={settings_url} title="Settings"
-					><div class="w-[{icon_size}px]">
-						<Settings
-							width={icon_size}
-							height={icon_size}
-							tailwindFillClass={'fill-lavandaLight dark:fill-purpleLight'}
-							tailwindStrokeClass={'stroke-black dark:stroke-white'}
-						/>
-					</div>
+				<a class:active={$page.url.pathname === settings_url} href={settings_url} title="Settings">
+					<Settings
+						tailwindFillClass={'fill-lavandaLight dark:fill-purpleLight'}
+						tailwindStrokeClass={'stroke-black dark:stroke-white'}
+					/>
 					<span>Settings</span></a
 				>
 			</li>
 			<li>
-				<a class:active={$page.url.pathname === logout_url} href={logout_url}
-					><div class="w-[{icon_size}px]">
-						<LogOut
-							width={icon_size}
-							height={icon_size}
-							tailwindFillClass={'fill-lavandaLight dark:fill-purpleLight'}
-							tailwindStrokeClass={'stroke-black dark:stroke-white'}
-						/>
-					</div>
+				<a class:active={$page.url.pathname === logout_url} href={logout_url}>
+					<LogOut
+						tailwindFillClass={'fill-lavandaLight dark:fill-purpleLight'}
+						tailwindStrokeClass={'stroke-black dark:stroke-white'}
+					/>
 					<span>Log Out</span></a
 				>
 			</li>
 		{:else}
 			<li>
-				<a class:active={$page.url.pathname === login_url} href={login_url}
-					><div class="w-[{icon_size}px]">
-						<LogIn
-							width={icon_size}
-							height={icon_size}
-							tailwindFillClass={'fill-lavandaLight dark:fill-purpleLight'}
-							tailwindStrokeClass={'stroke-black dark:stroke-white'}
-						/>
-					</div>
+				<a class:active={$page.url.pathname === login_url} href={login_url}>
+					<LogIn
+						tailwindFillClass={'fill-lavandaLight dark:fill-purpleLight'}
+						tailwindStrokeClass={'stroke-black dark:stroke-white'}
+					/>
 					<span>Log In</span></a
 				>
 			</li>
@@ -109,5 +88,8 @@
 	}
 	nav ul li a.active :global(path) {
 		stroke: white;
+	}
+	nav ul li a :global(svg) {
+		@apply w-[45px] h-[45px] sm:w-[60px] sm:h-[60px];
 	}
 </style>
