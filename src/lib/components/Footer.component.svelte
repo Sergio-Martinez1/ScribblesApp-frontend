65514af59726ad784da0dc80f2096e8a84958e18
SergioD <sergio_david2109@hotmail.com>
2024-06-02 23:03:12 -0600

    refactor: remove unusable pages

--- src/lib/components/Footer.component.svelte
+++ src/lib/components/Footer.component.svelte
@@ -1,23 +1,23 @@
-<div class="py-2.5 px-4 flex flex-col items-center bg-lavandaGray dark:bg-purpleGray rounded-2xl m-auto h-fit w-full">
+<div class="py-2.5 px-4 flex flex-col items-center bg-lavandaGray dark:bg-purpleGray rounded-2xl m-auto h-fit w-full shadow-[0_1px_2px_1px_rgba(0,0,0,0.15)]">
 	<h1 class="dark:text-white text-lg font-bold">Scribbles Team</h1>
 	<div class="flex flex-col items-center">
-		<span class="dark:text-white">user@mail.com</span>
+		<span class="dark:text-white">sdmzgz@gmail.com</span>
 		<span class="dark:text-white">user@mail.com</span>
 	</div>
-	<div class="flex flex-row gap-x-2 items-center justify-center flex-wrap">
-		<a href="/privacy">
-			<span class="dark:text-white">Privacy</span>
-		</a>
-		<div class="flex gap-x-2">
-			<span class="dark:text-white">&middot;</span>
-			<a href="/cookies">
-				<span class="dark:text-white">Cookies</span>
-			</a>
-		</div>
-		<div class="flex gap-x-2">
-			<span class="dark:text-white">&middot;</span>
-			<span class="dark:text-white">&copy;</span>
-			<span class="dark:text-white">2023</span>
-		</div>
-	</div>
+	<!-- <div class="flex flex-row gap-x-2 items-center justify-center flex-wrap"> -->
+	<!-- 	<a href="/privacy"> -->
+	<!-- 		<span class="dark:text-white">Privacy</span> -->
+	<!-- 	</a> -->
+	<!-- 	<div class="flex gap-x-2"> -->
+	<!-- 		<span class="dark:text-white">&middot;</span> -->
+	<!-- 		<a href="/cookies"> -->
+	<!-- 			<span class="dark:text-white">Cookies</span> -->
+	<!-- 		</a> -->
+	<!-- 	</div> -->
+	<!-- 	<div class="flex gap-x-2"> -->
+	<!-- 		<span class="dark:text-white">&middot;</span> -->
+	<!-- 		<span class="dark:text-white">&copy;</span> -->
+	<!-- 		<span class="dark:text-white">2023</span> -->
+	<!-- 	</div> -->
+	<!-- </div> -->
 </div>
