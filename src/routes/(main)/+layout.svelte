bb1e2a2f361cd003390ed8435050f934993e61ab
SergioD <sergio_david2109@hotmail.com>
2024-06-02 23:09:49 -0600

    style: improve layout display

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -7,11 +7,11 @@
 	$: isLogin = myUser ? true : false;
 </script>
 
-<div class="bg-lavandaDark dark:bg-purpleDark min-h-screen relative">
+<div class="bg-lavandaDark dark:bg-purpleDark min-h-screen">
 	<div
-		class="grid grid-cols-7 sm:grid-cols-9 md:grid-cols-12 px-[20px] md:px-[20px] lg:px-[40px] sm:gap-[20px] max-w-7xl mx-auto relative"
+		class="grid grid-cols-7 sm:grid-cols-9 md:grid-cols-12 px-[20px] md:px-[20px] lg:px-[40px] sm:gap-[20px] max-w-7xl mx-auto min-h-screen"
 	>
-		<div class="hidden sm:inline-block sm:col-span-2 top-[40px] sm:sticky h-fit">
+		<div class="hidden sm:inline-block sm:col-span-2 top-[40px] sm:sticky h-fit pb-5">
 			<NavBar login={isLogin} />
 		</div>
 		<slot><!-- optional fallback --></slot>
