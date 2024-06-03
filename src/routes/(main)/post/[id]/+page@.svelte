0a9aa13e069f12d724bf4203367429005418ee7a
SergioD <sergio_david2109@hotmail.com>
2024-06-02 23:15:23 -0600

    feat: improve view and enhance form actions

--- src/routes/(main)/post/[id]/+page@.svelte
+++ src/routes/(main)/post/[id]/+page@.svelte
@@ -1,21 +1,35 @@
 <script lang="ts">
-	import PostDetail from '$components/PostDetail.component.svelte';
 	import Comments from '$components/Comments.component.svelte';
 	import CommentPublicactionBar from '$components/CommentPublicactionBar.component.svelte';
+	import settings from '../../../../stores/settings';
+	import { afterNavigate, goto } from '$app/navigation';
+	import { Logo } from '$components/Icon';
+	import ArrowLeft from '$components/Icon/ArrowLeft.svelte';
+	import PostDetailDescription from '$components/PostDetailDescription.componente.svelte';
+	import PostDetailThumbnail from '$components/PostDetailThumbnail.component.svelte';
+	import PostDetailReactions from '$components/PostDetailReactions.component.svelte';
+	import InsatisfiedFace from '$components/Icon/InsatisfiedFace.svelte';
+	import type { ActionData } from './$types';
 	import type { PageData } from './$types';
-	import { page } from '$app/stores';
 
-	//FALTA IMPLEMENTAR LAS REACCIONES
-
+	export let form: ActionData;
 	export let data: PageData;
+
 	$: plainMyUser = data.plainMyUser;
-	$: isLogin = plainMyUser ? true : false;
 	$: id = plainMyUser && plainMyUser.id ? Number(plainMyUser.id) : -1;
 	$: profile_photo =
 		plainMyUser && plainMyUser.profile_photo !== 'null' ? plainMyUser.profile_photo : '';
-	$: my_reactions = 'my_reactions' in data ? data.my_reactions : null;
+	$: myReactions = data.myReactions;
 
-	function handleLike(my_reactions: any[], post_id: number) {
+	afterNavigate(({ from }) => {
+		$settings.previousPage = from?.url.toString() || '';
+	});
+
+	const navigateTo = (url: string) => {
+		goto(url, { noScroll: true, invalidateAll: false });
+	};
+
+	function handleLike(my_reactions: any[], post_id: number | undefined) {
 		if (my_reactions) {
 			let reaction_exits = my_reactions.filter((reaction) => reaction.post_id == post_id);
 			if (reaction_exits.length > 0) {
@@ -26,61 +40,215 @@
 	}
 </script>
 
-<div class="bg-lavandaDark dark:bg-purpleGray min-h-screen relative">
-	<div class="grid grid-cols-9 lg:grid-cols-12 lg:pr-[40px] lg:gap-[20px] mx-auto relative">
-		<div class="col-span-9 h-screen lg:sticky lg:top-0">
-			{#await data.streamed?.post}
-				<PostDetail loading={true} />
-			{:then post}
-				{#if post?.status === 200 && post.data}
-					<PostDetail
-						post_thumbnail_url={post.data.thumbnail ? post.data.thumbnail : ''}
-						post_content={post.data.content}
-						post_creation_date={post.data.publication_date}
-						user_photo_url={post.data.user.profile_photo}
-						user_name={post.data.user.username}
-						post_id={post.data.id}
-					/>
-				{:else if post?.status === 404}
-					<div class="bg-lavandaGray dark:bg-purpleGray rounded-2xl flex justify-center p-3">
-						<p class="dark:text-white">Post Not found</p>
-					</div>
+{#await data.streamed.post}
+	<div
+		class="min-h-screen bg-lavandaDark dark:bg-purpleDark pb-[81px] md:pb-0 md:pt-[61px] md:realtive md:h-screen md:grid md:grid-cols-[1fr_360px]"
+	>
+		<header
+			class="h-fit py-2 bg-lavandaGray dark:bg-purpleGray md:fixed md:top-0 md:w-full md:z-50"
+		>
+			<nav class="grid grid-cols-[70px_1fr_70px] h-[45px]">
+				{#if $settings.previousPage.length}
+					<button
+						on:click={() => {
+							navigateTo($settings.previousPage);
+						}}
+						class="bg-krispyPurple hover:bg-lessLavanda dark:hover:bg-lessPurple active:bg-krispyPurple rounded-br-3xl rounded-tr-3xl w-10 h-[45px] flex items-center justify-center"
+						><ArrowLeft width={30} height={30} /></button
+					>
 				{:else}
-					<div class="bg-lavandaGray dark:bg-purpleGray rounded-2xl flex justify-center p-3">
-						<p class="dark:text-white">Please reload the page</p>
-					</div>
+					<a href="/home" class="justify-self-center"><Logo width={45} height={45} /></a>
 				{/if}
-			{/await}
-		</div>
-
-		<div class="col-span-9 px-5 mt-4 lg:px-0 lg:col-span-3 lg:mt-8">
-			<p class="dark:text-white font-bold text-xl w-fit mx-auto mb-4">Comments</p>
-			<div class="mb-4">
-				<CommentPublicactionBar my_user_photo={profile_photo} post_id={Number($page.params.id)} />
+				<div
+					class="w-[105px] h-[25px] self-center justify-self-center bg-lavandaLight dark:bg-purpleLight rounded-full animate-pulse"
+				></div>
+			</nav>
+		</header>
+		<main class="h-full">
+			<PostDetailThumbnail isLoading={true} />
+			<section class="md:hidden overflow-scroll">
+				<PostDetailDescription isLoading={true} {form} />
+				<PostDetailReactions {form} />
+				<Comments isLoading={true} />
+			</section>
+		</main>
+		<aside class="hidden md:inline-block overflow-y-auto pb-[79px] relative">
+			<PostDetailDescription isLoading={true} {form} />
+			<PostDetailReactions {form} />
+			<Comments isLoading={true} />
+			<div class="hidden md:inline-block fixed bottom-0 w-[360px]">
+				<CommentPublicactionBar isLoading={true} />
 			</div>
-			<div class="mb-4">
-				{#await data.streamed?.comments}
-					<Comments loading={true} />
-				{:then comments}
-					{#if comments?.status === 200 && comments.data}
-						<Comments
-							comments={comments.data}
-							{isLogin}
-							my_user_photo={profile_photo}
-							post_id={Number($page.params.id)}
-							my_user_id={id}
-						/>
-					{:else if comments?.status === 404}
-						<div class="bg-lavandaGray dark:bg-purpleGray rounded-2xl flex justify-center p-3">
-							<p class="dark:text-white">No comments to see...</p>
-						</div>
-					{:else}
-						<div class="bg-lavandaGray dark:bg-purpleGray rounded-2xl flex justify-center p-3">
-							<p class="dark:text-white">Please reload the page</p>
-						</div>
-					{/if}
-				{/await}
-			</div>
-		</div>
+		</aside>
+		<footer class="md:hidden fixed bottom-0 w-full bg-lavandaGray dark:bg-purpleGray px-4 py-3">
+			<CommentPublicactionBar isLoading={true} />
+		</footer>
 	</div>
-</div>
+{:then postData}
+	{#if postData?.status !== 200}
+		<div class="min-h-screen bg-lavandaDark dark:bg-purpleDark">
+			<header class="w-full h-fit py-2 bg-lavandaGray dark:bg-purpleGray fixed">
+				<nav class="grid grid-cols-[70px_1fr_70px] h-[45px]">
+					{#if $settings.previousPage.length}
+						<button
+							on:click={() => {
+								navigateTo($settings.previousPage);
+							}}
+							class="bg-krispyPurple hover:bg-lessLavanda dark:hover:bg-lessPurple active:bg-krispyPurple rounded-br-3xl rounded-tr-3xl w-10 h-[45px] flex items-center justify-center"
+							><ArrowLeft width={30} height={30} /></button
+						>
+					{:else}
+						<a href="/home" class="justify-self-center"><Logo width={45} height={45} /></a>
+					{/if}
+					<span class="font-bold self-center justify-self-center dark:text-white">Scribbles</span>
+				</nav>
+			</header>
+			<main class="h-screen w-full flex flex-col items-center justify-center">
+				<div class="opacity-40">
+					<InsatisfiedFace
+						width={60}
+						height={60}
+						tailwindFillClass={'fill-black dark:fill-white'}
+					/>
+				</div>
+				{#if postData?.status === 404}
+					<span class="opacity-40 font-bold dark:text-white px-4 text-center">
+						Post not found, please return to Home page.
+					</span>
+				{:else}
+					<span class="opacity-40 font-bold dark:text-white px-4 text-center">
+						An unexpected has ocurred, please return to Home page.
+					</span>
+				{/if}
+			</main>
+		</div>
+	{:else}
+		{@const post = postData.data}
+		{@const post_thumbnail = postData.data?.thumbnail}
+		<div
+			class="min-h-screen bg-lavandaDark dark:bg-purpleDark pb-[81px] md:pb-0 md:pt-[61px] md:realtive md:h-screen grid max-md:grid-rows-[61px_1fr] grid-cols-[1fr] {post_thumbnail
+				? 'md:grid-cols-[1fr_360px]'
+				: 'md:grid-cols-[550px] justify-center'}"
+		>
+			<header
+				class="h-fit py-2 bg-lavandaGray dark:bg-purpleGray md:fixed md:top-0 md:w-full md:z-50 shadow-[0_1px_2px_1px_rgba(0,0,0,0.15)]"
+			>
+				<nav class="grid grid-cols-[70px_1fr_70px] h-[45px]">
+					{#if $settings.previousPage.length}
+						<button
+							on:click={() => {
+								navigateTo($settings.previousPage);
+							}}
+							class="bg-krispyPurple hover:bg-lessLavanda dark:hover:bg-lessPurple active:bg-krispyPurple rounded-br-3xl rounded-tr-3xl w-10 h-[45px] flex items-center justify-center"
+							><ArrowLeft width={30} height={30} /></button
+						>
+					{:else}
+						<a href="/home" class="justify-self-center"><Logo width={45} height={45} /></a>
+					{/if}
+					<span class="font-bold self-center justify-self-center dark:text-white"
+						>{post?.user.username}'s post</span
+					>
+				</nav>
+			</header>
+			<main
+				class="bg-lavandaGray dark:bg-purpleGray shadow-[0_1px_2px_1px_rgba(0,0,0,0.15)] md:rounded-2xl {post_thumbnail
+					? ''
+					: 'my-2 md:m-2'}"
+			>
+				<PostDetailThumbnail post_thumbnail_url={post_thumbnail} />
+				<section class="md:{post_thumbnail ? 'hidden' : ''} overflow-y-auto md:rounded-2xl">
+					<PostDetailDescription
+						{form}
+						post_id={post?.id}
+						post_creation_date={post?.publication_date}
+						user_photo_url={post?.user.profile_photo}
+						user_name={post?.user.username}
+						tags={post?.tags}
+						post_thumbnail_url={post?.thumbnail}
+						post_content={post?.content}
+						creator_id={post?.user.id}
+						myUser_id={id}
+						dont_show_dialog_id={`dontShowPostDetailDialog-${post?.id}-mobile`}
+						edit_dialog_id={`editPostDetailDialog-${post?.id}-mobile`}
+						delete_dialog_id={`deletePostDetailDialog-${post?.id}-mobile`}
+					/>
+					<PostDetailReactions
+						like_on={handleLike(myReactions, post?.id)}
+						likes_count={post?.reactions ? post?.reactions.length : 0}
+						comments_count={post?.num_comments ? post?.num_comments : 0}
+						tags_count={post?.tags ? post?.tags.length : 0}
+						post_url="/post/{post?.id}"
+						tags={post?.tags ? post?.tags.map((tag) => tag?.content) : []}
+						post_by_tags_url="/home"
+						post_id={post?.id}
+						my_user_id={id}
+						{form}
+					/>
+					{#await data.streamed.comments}
+						<Comments isLoading={true} />
+					{:then commentsData}
+						<Comments
+							comments={commentsData.data}
+							my_user_id={id}
+							uniqueIdentifier={'mobile'}
+							post_id={post?.id}
+						/>
+					{/await}
+					<footer
+						class="fixed bottom-0 w-full md:w-[534px] bg-lavandaGray dark:bg-purpleGray px-4 py-3 md:rounded-b-2xl md:mb-2"
+					>
+						<CommentPublicactionBar my_user_photo={profile_photo} post_id={post?.id} />
+					</footer>
+				</section>
+			</main>
+			<aside
+				class="hidden md:{post_thumbnail
+					? 'inline-block'
+					: 'hidden'} overflow-y-auto pb-[79px] relative rounded-2xl m-2 shadow-[0_1px_2px_1px_rgba(0,0,0,0.15)] bg-lavandaGray dark:bg-purpleGray"
+			>
+				<PostDetailDescription
+					{form}
+					post_id={post?.id}
+					post_creation_date={post?.publication_date}
+					user_photo_url={post?.user.profile_photo}
+					user_name={post?.user.username}
+					tags={post?.tags}
+					post_thumbnail_url={post?.thumbnail}
+					post_content={post?.content}
+					creator_id={post?.user.id}
+					myUser_id={id}
+					dont_show_dialog_id={`dontShowPostDetailDialog-${post?.id}-desktop`}
+					edit_dialog_id={`editPostDetailDialog-${post?.id}-desktop`}
+					delete_dialog_id={`deletePostDetailDialog-${post?.id}-desktop`}
+				/>
+				<PostDetailReactions
+					like_on={handleLike(myReactions, post?.id)}
+					likes_count={post?.reactions ? post?.reactions.length : 0}
+					comments_count={post?.num_comments ? post?.num_comments : 0}
+					tags_count={post?.tags ? post?.tags.length : 0}
+					post_url="/post/{post?.id}"
+					tags={post?.tags ? post?.tags.map((tag) => tag.content) : []}
+					post_by_tags_url="/home"
+					post_id={post?.id}
+					my_user_id={id}
+					{form}
+				/>
+				{#await data.streamed.comments}
+					<Comments isLoading={true} />
+				{:then commentsData}
+					<Comments
+						comments={commentsData.data}
+						my_user_id={id}
+						uniqueIdentifier={'desktop'}
+						post_id={post?.id}
+					/>
+				{/await}
+				<div
+					class="fixed bottom-0 w-[344px] px-4 py-3 bg-lavandaGray dark:bg-purpleGray rounded-b-2xl mb-2"
+				>
+					<CommentPublicactionBar my_user_photo={profile_photo} post_id={post?.id} />
+				</div>
+			</aside>
+		</div>
+	{/if}
+{/await}
